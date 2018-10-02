acb00a4a29d6a4495901e174d6c49fb7a4b84cbb
saileshkasaju <leosailesh@gmail.com>
2018-10-02 17:08:19 +0545

    add dashboard

---
+++ client/app/containers/Dashboard/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import { Dashboard } from '../index';
+
+describe('<Dashboard />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
