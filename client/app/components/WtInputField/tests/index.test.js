12b1d683b4a9d4eafef4ff57a267b77720ba680d
saileshkasaju <leosailesh@gmail.com>
2018-10-07 12:47:09 +0545

    add template input types

---
+++ client/app/components/WtInputField/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import WtInputField from '../index';
+
+describe('<WtInputField />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
