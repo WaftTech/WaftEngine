c4f088f4c1530c4f7802b261f43c5c783ba99833
Manoj Kumar Mahato <mkmpvtltd@gmail.com>
2018-10-08 11:38:10 +0545

    update

--- server/modules/Module/ModuleController.js
+++ server/modules/Module/ModuleController.js
@@ -5,27 +5,131 @@ const ModuleSch = require('./Module');
 const moduleController = {};
 const internal = {};
 
+internal.validateEmail = email => {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
 internal.checkField = (res, k, v) => {
   console.log('Field', k, '-', v);
   const fieldTypes = Object.keys(moduleConfig.fieldType);
   if (!v.label) {
-    return otherHelper.sendResponse(res, HttpStatus.FAILED_DEPENDENCY, false, null, `${k} field must have label`);
+    return otherHelper.sendResponse(res, HttpStatus.FAILED_DEPENDENCY, false, null, `'${k}' must have 'label'`);
   }
-  if (!v.type) {
-    return otherHelper.sendResponse(res, HttpStatus.FAILED_DEPENDENCY, false, null, `${k} field must have type`);
-  } else if (!fieldTypes.includes(v.type)) {
-    return otherHelper.sendResponse(res, HttpStatus.FAILED_DEPENDENCY, false, null, `${k} field must have valid type`);
+  if (!v.key) {
+    return otherHelper.sendResponse(res, HttpStatus.FAILED_DEPENDENCY, false, null, `'${k}' must have 'key'`);
+  }
+  if (!v.name) {
+    return otherHelper.sendResponse(res, HttpStatus.FAILED_DEPENDENCY, false, null, `'${k}' must have 'name'`);
+  }
+  v.required = v.required == 'true' ? true : v.required == true ? true : false;
+  if (!fieldTypes.includes(k)) {
+    return otherHelper.sendResponse(res, HttpStatus.FAILED_DEPENDENCY, false, null, `'${k}' is not valid type`);
+  } else {
+    switch (k) {
+      case 'SingleLine':
+        if ('' + v.numberOfCharacter && !isNaN(parseInt(v.numberOfCharacter)) && parseInt(v.numberOfCharacter) < 256) {
+          v.numberOfCharacter = parseInt(v.numberOfCharacter);
+        } else {
+          return otherHelper.sendResponse(res, HttpStatus.FAILED_DEPENDENCY, false, null, `key 'numberOfCharacter' is not found or valid value for label '${v.label}'`);
+        }
+        break;
+      case 'AutoNumberField':
+        console.log('v.startingNumber', v.startingNumber);
+        if ('' + v.startingNumber && !isNaN(parseInt(v.startingNumber))) {
+          v.startingNumber = parseInt(v.startingNumber);
+        } else {
+          return otherHelper.sendResponse(res, HttpStatus.FAILED_DEPENDENCY, false, null, `key 'startingNumber' is not found or valid value for label '${v.label}'`);
+        }
+        break;
+      case 'MultiLine':
+        if ('' + v.numberOfCharacter && !isNaN(parseInt(v.numberOfCharacter))) {
+          v.numberOfCharacter = parseInt(v.numberOfCharacter);
+        }
+        break;
+      case 'Email':
+        v.duplicate = v.duplicate == 'true' ? true : v.duplicate == true ? true : false;
+        break;
+      case 'Phone':
+        v.duplicate = v.duplicate == 'true' ? true : v.duplicate == true ? true : false;
+        break;
+      case 'PickList':
+        if (v.option && typeof v.option === 'object' && v.option.length > 0) {
+        } else {
+          return otherHelper.sendResponse(res, HttpStatus.FAILED_DEPENDENCY, false, null, `key 'option' is not found or valid array of string for label '${v.label}'`);
+        }
+        if (!v.default) {
+          //TODO: check value is from option or not
+          return otherHelper.sendResponse(res, HttpStatus.FAILED_DEPENDENCY, false, null, `key 'default' is not found or valid string for label '${v.label}'`);
+        }
+        break;
+      case 'MultiSelect':
+        if (v.option && typeof v.option === 'object' && v.option.length > 0) {
+        } else {
+          return otherHelper.sendResponse(res, HttpStatus.FAILED_DEPENDENCY, false, null, `key 'option' is not found or valid array of string for label '${v.label}'`);
+        }
+        if (v.default && typeof v.default === 'object' && v.default.length > -1) {
+        } else {
+          return otherHelper.sendResponse(res, HttpStatus.FAILED_DEPENDENCY, false, null, `key 'default' is not found or valid array of string for label '${v.label}'`);
+        }
+        break;
+      case 'Date':
+        if (v.default) {
+        } else {
+          //TODO: check value is from option or not
+          return otherHelper.sendResponse(res, HttpStatus.FAILED_DEPENDENCY, false, null, `key 'default' is not found or valid string for label '${v.label}'`);
+        }
+        break;
+      case 'DateTime':
+        if (v.default) {
+        } else {
+          //TODO: check value is from option or not
+          return otherHelper.sendResponse(res, HttpStatus.FAILED_DEPENDENCY, false, null, `key 'default' is not found or valid string for label '${v.label}'`);
+        }
+        break;
+      case 'Decimal':
+        if ('' + v.default && !isNaN(parseFloat(v.default))) {
+          v.default = parseFloat(v.default);
+        } else {
+          return otherHelper.sendResponse(res, HttpStatus.FAILED_DEPENDENCY, false, null, `key 'default' is not found or valid value for label '${v.label}'`);
+        }
+        if ('' + v.maxDigit && !isNaN(parseInt(v.maxDigit)) && parseInt(v.maxDigit) <= 16) {
+          v.maxDigit = parseInt(v.maxDigit);
+        } else {
+          return otherHelper.sendResponse(res, HttpStatus.FAILED_DEPENDENCY, false, null, `key 'maxDigit' is not found or valid value or less than 17 for label '${v.label}'`);
+        }
+        if ('' + v.decimalPlace && !isNaN(parseInt(v.decimalPlace)) && parseInt(v.decimalPlace) <= 9) {
+          v.decimalPlace = parseInt(v.decimalPlace);
+        } else {
+          return otherHelper.sendResponse(res, HttpStatus.FAILED_DEPENDENCY, false, null, `key 'decimalPlace' is not found or valid value or less than 10 for label '${v.label}'`);
+        }
+        break;
+      case 'LongInteger':
+        break;
+      case 'Checkbox':
+        break;
+      case 'URL':
+        break;
+      case 'Lookup':
+        break;
+      case 'MultiSelectLookup':
+        break;
+      case 'User':
+        break;
+      case 'FileUpload':
+        break;
+      default:
+    }
   }
 };
 internal.checkSection = (res, k, v) => {
   console.log('Section', k, '-', v);
   if (!v.name) {
-    return otherHelper.sendResponse(res, HttpStatus.FAILED_DEPENDENCY, false, null, 'Section must have label name');
+    return otherHelper.sendResponse(res, HttpStatus.FAILED_DEPENDENCY, false, null, `Section must have label name`);
   }
   if (!v.layout) {
-    return otherHelper.sendResponse(res, HttpStatus.FAILED_DEPENDENCY, false, null, 'Section must have layout selected');
+    return otherHelper.sendResponse(res, HttpStatus.FAILED_DEPENDENCY, false, null, `Section must have layout selected`);
   } else if (!moduleConfig.section.layout.includes(v.layout)) {
-    return otherHelper.sendResponse(res, HttpStatus.FAILED_DEPENDENCY, false, null, 'Section must have valid layout selected');
+    return otherHelper.sendResponse(res, HttpStatus.FAILED_DEPENDENCY, false, null, `Section must have valid layout selected`);
   }
   if (v.fields && v.fields.length) {
     v.fields.forEach((elem, index) => {
@@ -42,7 +146,6 @@ internal.checkSection = (res, k, v) => {
   }
 };
 moduleController.getFieldConfig = async (req, res, next) => {
-  console.log(1);
   return otherHelper.sendResponse(res, HttpStatus.OK, true, moduleConfig, null, 'Module Config', null);
 };
 moduleController.validateModuleConfig = async (req, res, next) => {
@@ -50,7 +153,7 @@ moduleController.validateModuleConfig = async (req, res, next) => {
     const section = req.body.section;
     section.forEach((elem, index) => {
       for (let [k, v] of Object.entries(elem)) {
-        if (v.type === 'Section') {
+        if (k === 'Section') {
           internal.checkSection(res, k, v);
         } else {
           internal.checkField(res, k, v);
@@ -72,8 +175,6 @@ moduleController.saveModuleConfig = async (req, res, next) => {
     const {
       body: { module_name, _id, is_active, section },
     } = req;
-
-    console.log(2);
     let response;
     let moduleSch = await ModuleSch.findOne({ module_key: module_key });
     if (moduleSch && moduleSch._id) {
@@ -92,8 +193,7 @@ moduleController.saveModuleConfig = async (req, res, next) => {
       });
       response = await moduleSch.save();
     }
-    return otherHelper.sendResponse(res, HttpStatus.OK, true, response, null, `${module_name} Module Setting Saved`, null);
-    // return otherHelper.sendResponse(res, HttpStatus.OK, true, req.body, null, `${module_name} Module Setting Saved`, null);
+    return otherHelper.sendResponse(res, HttpStatus.OK, true, response, null, `'${module_name}' Module Setting Saved`, null);
   } catch (err) {
     return err;
   }
