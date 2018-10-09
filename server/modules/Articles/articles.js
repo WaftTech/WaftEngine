3fd6cdaf8aab08b4054f62077576c4ed2a01adcc
Manoj Kumar Mahato <mkmpvtltd@gmail.com>
2018-10-09 17:18:10 +0545

    update for delete

--- server/modules/Articles/articles.js
+++ server/modules/Articles/articles.js
@@ -4,13 +4,17 @@ const { slugify } = require('../../helper/others.helper');
 
 const ArticleSchema = new Schema({
   ArticleName: { type: String, required: true },
-  slug: { type: String, required: true, unique: true },
+  slug: { type: String, unique: true },
   Description: { type: String, required: false },
   PublishFrom: { type: Date, required: true },
   IsActive: { type: Boolean, required: true, default: false },
   IsFeature: { type: Boolean, required: true, default: false },
   ArticleImage: { type: String, required: true },
+  IsDeleted: { type: Boolean, required: true, default: false },
+  Added_by: { type: Schema.Types.ObjectId },
   Added_at: { type: Date, default: Date.now },
+  Deleted_by: { type: Schema.Types.ObjectId },
+  Deleted_at: { type: Date },
 });
 // on every save, add the date
 ArticleSchema.pre('save', function(next) {
