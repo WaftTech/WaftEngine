7f65c3213e804f4e81db6e94655a2f60425c4d3e
Manoj Kumar Mahato <mkmpvtltd@gmail.com>
2018-10-09 16:31:42 +0545

    Organization delete and get by

--- server/modules/Organization/organizationController.js
+++ server/modules/Organization/organizationController.js
@@ -1,11 +1,12 @@
 const HttpStatus = require('http-status');
+var ObjectId = require('mongoose').Types.ObjectId;
 const otherHelper = require('../../helper/others.helper');
 const OrgSch = require('./organization');
 const orgController = {};
 const internal = {};
 
 orgController.GetOrganization = async (req, res, next) => {
-  const orgs = await OrgSch.find();
+  const orgs = await OrgSch.find({ IsDeleted: false });
   return otherHelper.sendResponse(res, HttpStatus.OK, true, orgs, null, 'Organization Get Success !!', null);
 };
 orgController.SaveOrganization = async (req, res, next) => {
@@ -34,8 +35,19 @@ orgController.SaveOrganization = async (req, res, next) => {
 };
 orgController.GetOrganizationDetail = async (req, res, next) => {
   const slug = req.params.slug;
-  const org = await OrgSch.findOne({ slug: slug });
+  const org = await OrgSch.findOne({ slug: slug, IsDeleted: false });
   return otherHelper.sendResponse(res, HttpStatus.OK, true, org, null, 'Organization Get Success !!', null);
+};
+orgController.GetOrganizationDetailByID = async (req, res, next) => {
+  const id = req.params.id;
+  const org = await OrgSch.findOne({ _id: ObjectId(id), IsDeleted: false });
+  return otherHelper.sendResponse(res, HttpStatus.OK, true, org, null, 'Organization Get Success !!', null);
+};
+orgController.DeleteOrganization = async (req, res, next) => {
+  const id = req.params.id;
+  const result = await OrgSch.findByIdAndUpdate(id, { $set: { IsDeleted: true, Deleted_by: req.user.id, Deleted_at: new Date() } });
+
+  return otherHelper.sendResponse(res, HttpStatus.OK, true, result, null, 'Organization Deleted !!', null);
 };
 
 module.exports = orgController;
