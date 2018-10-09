5a2dd0c705ebe833b3f88fcd4979919aa3325cfd
jivanshr <jivanshr@gmail.com>
2018-10-09 17:27:33 +0545

    commiting just to pull

--- client/app/containers/HomePage/index.js
+++ client/app/containers/HomePage/index.js
@@ -20,16 +20,28 @@ import styled from "styled-components";
 
 
 const Masthead =  styled.div`
-background:#333 url(assets/img/background.jpg) no-repeat center top; 
+background:#690 url(../assets/img/logo.png) center top; 
 background-attachement:fixed;
 height:350px; padding-top:20px; color:#fff;
 `;
+
+
+const CategoryItem =  styled.div`
+box-shadow: 0 5px 5px rgba(0, 0, 0, 0.2);
+border-radius: 200px;
+padding: 30px 50px;
+`;
+
+
+
+
+
 
 /* eslint-disable react/prefer-stateless-function */
 export default class HomePage extends React.PureComponent {
     render() {
         return (
-            <section className="el-container is-vertical">
+            <section className="">
                 <header className="el-header header-main">
                     <div className="container">
                         <div className="logo">
@@ -50,6 +62,16 @@ export default class HomePage extends React.PureComponent {
                     </div>
                 </Masthead>
 
+
+                <CategoryItem>
+                    <div className="media-info">
+                    <div className="img">
+                    <img src={categoryIcon}/></div> 
+                    <div className="info">
+                    <strong><a href="#">Restaurant</a> </strong> 
+                    <span className="designation">4606</span></div></div>
+                
+                </CategoryItem>
 
                 <div className="card">
 <img src={rest1} />
