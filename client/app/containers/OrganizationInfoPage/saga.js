0267014b2e12295b8a4a25536d415b6c7629bb81
saileshkasaju <leosailesh@gmail.com>
2018-10-10 12:47:07 +0545

    image setup in org done

--- client/app/containers/OrganizationInfoPage/saga.js
+++ client/app/containers/OrganizationInfoPage/saga.js
@@ -26,7 +26,8 @@ function* loadOne(action) {
 function* addEdit(action) {
   const token = yield select(makeSelectToken());
   const { ProfileImage, ProfileImage1, ...data } = action.payload;
-  const files = {ProfileImage, ProfileImage1};
+  // const files = {ProfileImage, ProfileImage1};
+  const files = [ProfileImage, ProfileImage1];
   yield call(
     Api.multipartPost(
       'org',
