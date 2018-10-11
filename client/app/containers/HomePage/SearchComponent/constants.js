3efde5b1084df87f62a13d9c29b3cca8fcc7867e
saileshkasaju <leosailesh@gmail.com>
2018-10-11 17:48:00 +0545

    add boilerplate

--- client/app/containers/HomePage/SearchComponent/constants.js
+++ client/app/containers/HomePage/SearchComponent/constants.js
@@ -1,3 +1,7 @@
 export const LOAD_CATEGORIES_REQUEST = 'app/HomePage/SearchComponent/LOAD_CATEGORIES_REQUEST';
 export const LOAD_CATEGORIES_SUCCESS = 'app/HomePage/SearchComponent/LOAD_CATEGORIES_SUCCESS';
 export const LOAD_CATEGORIES_FAILURE = 'app/HomePage/SearchComponent/LOAD_CATEGORIES_FAILURE';
+
+export const SEARCH_REQUEST = 'app/HomePage/SearchComponent/SEARCH_REQUEST';
+export const SEARCH_SUCCESS = 'app/HomePage/SearchComponent/SEARCH_SUCCESS';
+export const SEARCH_FAILURE = 'app/HomePage/SearchComponent/SEARCH_FAILURE';
---
+++ client/app/containers/HomePage/actions.js
@@ -0,0 +1,38 @@
+export const loadCategoryRequest = payload => ({
+  type: types.LOAD_CATEGORY_REQUEST,
+  payload,
+});
+export const loadCategorySuccess = payload => ({
+  type: types.LOAD_CATEGORY_SUCCESS,
+  payload,
+});
+export const loadCategoryFailure = payload => ({
+  type: types.LOAD_CATEGORY_FAILURE,
+  payload,
+});
+
+export const loadFourorgRequest = payload => ({
+  type: types.LOAD_FOURORG_REQUEST,
+  payload,
+});
+export const loadFourorgSuccess = payload => ({
+  type: types.LOAD_FOURORG_SUCCESS,
+  payload,
+});
+export const loadFourorgFailure = payload => ({
+  type: types.LOAD_FOURORG_FAILURE,
+  payload,
+});
+
+export const loadOrgRequest = payload => ({
+  type: types.LOAD_ORG_REQUEST,
+  payload,
+});
+export const loadOrgSuccess = payload => ({
+  type: types.LOAD_ORG_SUCCESS,
+  payload,
+});
+export const loadOrgFailure = payload => ({
+  type: types.LOAD_ORG_FAILURE,
+  payload,
+});
