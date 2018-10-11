66f1266bd649b648e3075421fd5b1422984c7d52
Manoj Kumar Mahato <mkmpvtltd@gmail.com>
2018-10-11 11:53:20 +0545

    GEt organization for home page

--- server/routes/api/home.js
+++ server/routes/api/home.js
@@ -5,5 +5,6 @@ const dModule = require('../../modules/Home/HomeController');
 router.get('/category', dModule.GetCategories);
 router.get('/cat/:catid', dModule.GetOrganizationByCategoryById);
 router.get('/org/:orgslug', dModule.GetOrganizationDetailByslug);
+router.get('/fourorg', dModule.GetLatestFourOrganization);
 
 module.exports = router;
