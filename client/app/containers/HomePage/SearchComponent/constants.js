21f265f690618e846f4d2878c3db74811b5d358b
saileshkasaju <leosailesh@gmail.com>
2018-10-11 16:48:19 +0545

    add search component

---
+++ client/app/containers/HomePage/SearchComponent/constants.js
@@ -0,0 +1,3 @@
+export const LOAD_CATEGORIES_REQUEST = 'app/HomePage/SearchComponent/LOAD_CATEGORIES_REQUEST';
+export const LOAD_CATEGORIES_SUCCESS = 'app/HomePage/SearchComponent/LOAD_CATEGORIES_SUCCESS';
+export const LOAD_CATEGORIES_FAILURE = 'app/HomePage/SearchComponent/LOAD_CATEGORIES_FAILURE';
