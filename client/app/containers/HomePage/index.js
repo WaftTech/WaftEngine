50db5c0444d77fa0a45398be60a9e70b5ad1ae68
jivanshr <jivanshr@gmail.com>
2018-10-11 12:31:54 +0545

    homepage changes

--- client/app/containers/HomePage/index.js
+++ client/app/containers/HomePage/index.js
@@ -14,11 +14,27 @@ import { FormattedMessage } from 'react-intl';
 import messages from './messages';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import { Icon, Button, TextField, Tabs, Tab } from '@material-ui/core';
+import { Icon, Button, TextField, Paper } from '@material-ui/core';
 import logo from 'assets/img/logo.png';
 import background from 'assets/img/background.jpg';
 import rest1 from 'assets/img/rest1.jpg';
 import categoryIcon from 'assets/img/rest_thumb.png';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import { Search, CalendarToday, LocationOn, Timelapse, Call, Email, Wifi } from '@material-ui/icons';
+import { withStyles } from '@material-ui/core/styles';
+import aries from "assets/img/aries.png";
+
+
+function TabContainer(props) {
+    return (
+        <Typography component="div" style={{ padding: 8 * 3 }}>
+            {props.children}
+        </Typography>
+    );
+}
 
 const Masthead = styled.div`
   background: #333 url(${background}) no-repeat center center;
@@ -32,6 +48,7 @@ const Masthead = styled.div`
 const CategoryItem = styled.div`
 box-shadow: 0 5px 5px rgba(0, 0, 0, 0.2);
 padding: 30px 50px;
+margin-bottom:20px;
 `;
 
 
@@ -40,11 +57,21 @@ padding: 30px 50px;
 
 
 /* eslint-disable react/prefer-stateless-function */
-export default class HomePage extends React.PureComponent {
+export default class HomePage extends React.Component {
+    state = {
+        value: 0,
+    };
+
+    handleChange = (event, value) => {
+        this.setState({ value });
+    };
+
+
     render() {
+        const { value } = this.state;
         return (
-            <section className="el-container is-vertical">
-                <header className="el-header header-main">
+            <section className="is-vertical">
+                <header className="header header-main">
                     <div className="container">
                         <div className="logo">
                             <Link to="/">
@@ -59,6 +86,7 @@ export default class HomePage extends React.PureComponent {
                         <br />
                         <br />
                         <h2>Ask To Marina</h2>
+
                         <strong className="content-title">
                             BE A PART OF THE LOCAL SEARCH REVOLUTION
             </strong>
@@ -94,53 +122,69 @@ export default class HomePage extends React.PureComponent {
 
                     </CategoryItem>
 
-                    <br />
-                    <br />
-                    <br />
-                    <Tabs
-                    >
-                        <Tab
-                            disableRipple
-                            label="Video"
-                        />
-                        <Tab
-                            disableRipple
-                            label="About Us"
-                        />
-                        <Tab
-                            disableRipple
-                            label="Rashifal"
-                        />
-                    </Tabs>
+
+                    <div>
+                        <Paper>
+                            <Tabs value={value} onChange={this.handleChange} indicatorColor="primary"
+                                textColor="primary"
+                                centered>
+                                <Tab label="Recent Videos" />
+                                <Tab label="About Us" />
+                                <Tab label="Rashifal" />
+                            </Tabs>
+                        </Paper>
+                        {value === 0 && <TabContainer><div className="text-center"><iframe width="560" height="315" src="https://www.youtube.com/embed/a_wuykzfFzE" frameborder="0" allowFullScreen /></div></TabContainer>}
+                        {value === 1 && <TabContainer><div className="text-center"><div className="maxWidth500">A maggot wound is easily identifiable - A hole of any sort, a characteristic smell. You can actually see the maggots moving in the wound. But do not worry the treatment is simple. The vet cleans the area with antiseptic. They then applies Lorex­ene, or Maggocide, ointments specifically meant to kill maggots.</div></div> </TabContainer>}
+                        {value === 2 && <TabContainer>
+                            <div className="row">
+                                <div className="col-4">
+                                    <div className="card box-card is-always-shadow">
+                                        <div className="card__header"><div className="clearfix">
+                                            <span className="designation">मेष - चु, चे, चो, ला, लि, लु, ले, लो, अ (Aries)</span>
+                                        </div></div>
+                                        <div className="card__body">
+                                            <div className="text item media-info">
+                                                <div className="img"><img src={aries} height="60" width="60" />
+                                                </div> <div className="info"><span className="designation">
+                                                    Lorem ipsum dolor sit amet, consectetur
+                                                                                                      adipisicing elit. Proin nibh augue
+                                                  conseqaut nibbhi ellit ipsum consectetur.</span></div></div></div></div>
+                                </div>
+                            </div>
+                        </TabContainer>}
+                    </div>
 
                     <br />
-                    <br />
-                    <h2>Some of Organizations</h2>
+                    <h2 className="text-center">Some of Organizations</h2>
                     <div className="card">
-                        <img src={rest1} />
-                        <h3>Thakali Restaurant</h3>
-                        Opening Days:SUN, MON, TUE, WED, THU, FRI
-          <Icon>Star</Icon> 1234456
-          <Icon>Star</Icon> State 3 Bhaktapur Thimi,hanumante Bridge
-          <Icon>Star</Icon> Pincode: 123
-          <Icon>Star</Icon> Opening Time:10AM-7PM
-          <Icon>Star</Icon> 99999999999
-          <Icon>Star</Icon> info@email.com
-          <Icon>Star</Icon> website.com
-        </div>
-
-
-                    <div className="cta-block">
-                        <div className="container">
-                            <h2>Require something special?</h2>
-                            <p>We might help you to build it better way!</p>
-                            <Button variant="secondary" size="large">
-                                <span>Send us some words</span></Button></div></div>
-
-
-
+                        <div className="row">
+                            <div className="col-3">
+                                <img src={rest1} /></div>
+                            <div className="col-9">
+                                <br />
+                                <h5>Thakali Restaurant</h5>
+                                <CalendarToday />Opening Days:SUN, MON, TUE, WED, THU, FRI <br />
+                                <LocationOn /> State 3 Bhaktapur Thimi,hanumante Bridge<br />
+                                <Timelapse /> Opening Time:10AM-7PM<br />
+                                <Call /> 977-9849242008<br />
+                                <Email /> info@email.com<br />
+                                <Wifi />http://website.com
+          </div>
+                        </div>
+                    </div>
 
                 </div>
+                <div className="cta-block">
+                    <div className="container">
+                        <h2>Require something special?</h2>
+                        <p>We might help you to build it better way!</p>
+                        <Button variant="secondary" size="large">
+                            <span>Send us some words</span></Button></div></div>
+
+
+
+
+
 
             </section>
         );
