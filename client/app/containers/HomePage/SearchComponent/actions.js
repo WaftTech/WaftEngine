9e30fd9ae36568faa33c548490704c5f185b6d77
saileshkasaju <leosailesh@gmail.com>
2018-10-11 17:48:00 +0545

    add boilerplate

--- client/app/containers/HomePage/SearchComponent/actions.js
+++ client/app/containers/HomePage/SearchComponent/actions.js
@@ -10,3 +10,16 @@ export const loadCategoriesFailure = payload => ({
   type: types.LOAD_CATEGORIES_FAILURE,
   payload,
 });
+
+export const searchRequest = payload => ({
+  type: types.SEARCH_REQUEST,
+  payload,
+});
+export const searchSuccess = payload => ({
+  type: types.SEARCH_SUCCESS,
+  payload,
+});
+export const searchFailure = payload => ({
+  type: types.SEARCH_FAILURE,
+  payload,
+});
