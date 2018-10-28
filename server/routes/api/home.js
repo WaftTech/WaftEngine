8ffa0a016c15b5a1e64ab5b6e26d9ad585e1df16
Manoj Kumar Mahato <mkmpvtltd@gmail.com>
2018-10-28 12:36:38 +0545

    Remove unnecessary module

--- server/routes/api/home.js
+++ server/routes/api/home.js
@@ -3,9 +3,6 @@ const router = express.Router();
 const dModule = require('../../modules/Home/HomeController');
 
 router.get('/category', dModule.GetCategories);
-router.get('/cat/:catid', dModule.GetOrganizationByCategoryById);
-router.get('/org/:orgslug', dModule.GetOrganizationDetailByslug);
-router.get('/fourorg', dModule.GetLatestFourOrganization);
 router.get('/search/:catid/:text', dModule.GetDataforSearch);
 
 module.exports = router;
