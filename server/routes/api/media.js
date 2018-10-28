ec35864e6177f6b0dac8a551bc644ccbe6e9ff5e
Manoj Kumar Mahato <mkmpvtltd@gmail.com>
2018-10-28 17:20:22 +0545

    Media Section

---
+++ server/routes/api/media.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const upload = multer({ dest: 'public/media/' });
+
+const dModule = require('../../modules/Media/mediaController');
+const authenticationMiddleware = require('../../middleware/authentication.middleware');
+
+router.get('/', authenticationMiddleware.authorization, dModule.GetMedia);
+router.post('/single/:type', authenticationMiddleware.authorization, upload.array('file', 1), dModule.SaveMedia);
+router.get('/:id', dModule.GetMediaDetail);
+router.delete('/:id', authenticationMiddleware.authorization, dModule.DeleteMedia);
+
+module.exports = router;
