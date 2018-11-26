c888209d2a6c11f80516f16fc8e3b819a04165dc
saileshkasaju <leosailesh@gmail.com>
2018-11-26 10:22:39 +0545

    clean code

--- client-admin/node_modules/react-jss/lib/getDisplayName.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-exports['default'] = function (Component) {
-  return Component.displayName || Component.name || 'Component';
-};
