39f0e342412c0681644c5e92a6923258c5f7e2bd
Manoj Kumar Mahato <mkmpvtltd@gmail.com>
2018-11-26 18:17:38 +0545

    update all

--- server/routes/api/roles.js
+++ server/routes/api/roles.js
@@ -2,17 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 const dModule = require('../../modules/Roles/roleController');
-const authenticationMiddleware = require('../../middleware/authentication.middleware');
+const { authentication, authorization } = require('../../middleware/authentication.middleware');
 
-router.get('/', authenticationMiddleware.authorization, dModule.GetRoles);
-router.get('/:id', authenticationMiddleware.authorization, dModule.GetRoleDetail);
-router.post('/', authenticationMiddleware.authorization, dModule.AddRoles);
-router.get('/module', authenticationMiddleware.authorization, dModule.GetModule);
-router.get('/module/:id', authenticationMiddleware.authorization, dModule.GetModuleDetail);
-router.post('/module', authenticationMiddleware.authorization, dModule.AddModulList);
+router.get('/role', authorization, dModule.GetRoles);
+router.get('/role/:id', authorization, dModule.GetRoleDetail);
+router.post('/role', authorization, dModule.AddRoles);
+router.get('/module', authorization, dModule.GetModule);
+router.get('/module/:id', authorization, dModule.GetModuleDetail);
+router.post('/module', authorization, dModule.AddModulList);
 
-router.get('/routes', authenticationMiddleware.authorization, dModule.getAccessList);
-router.post('/routes', authenticationMiddleware.authorization, dModule.SaveAccessList);
+router.get('/access', authorization, dModule.getAccessList);
+router.post('/access', authorization, dModule.SaveAccessList);
 // router
 //   .get('/:name',  dModule.getModuleData)
 //   .post('/:name', dModule.saveModuleData);
