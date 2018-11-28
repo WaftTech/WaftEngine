10b88728eaa86a33aabd638a2e5b70dd937b3e9d
Manoj Kumar Mahato <mkmpvtltd@gmail.com>
2018-11-28 13:43:27 +0545

    update message for individual server

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.set('PORT_NUMBER', port);
 
 //  Start the app on the specific interface (and port).
 server.listen(port, () => {
-  console.log(`API Docs Server application started on port ${port} at Date ${new Date()}`);
+  console.log(`API Server started on port ${port} at Date ${new Date()}`);
 });
 
 process.on('SIGTERM', () => {
