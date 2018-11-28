b87ba76cf52446bafe430372b518c52fa9943997
Manoj Kumar Mahato <mkmpvtltd@gmail.com>
2018-11-28 13:08:26 +0545

    update for not server from api

--- server/app.js
+++ server/app.js
@@ -42,7 +42,7 @@ app.use(
 );
 app.use(cookieParser());
 app.use('/public', express.static(path.join(__dirname, 'public')));
-app.use('/', express.static(path.join(__dirname, '../client-user/build')));
+// app.use('/', express.static(path.join(__dirname, '../client-user/build')));
 
 // DB Config
 mongoose.Promise = global.Promise;
