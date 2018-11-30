28b8549c1df091dc1046d4c34924313ab177d821
Manoj Kumar Mahato <mkmpvtltd@gmail.com>
2018-11-30 16:25:44 +0545

    Update for Access Manage

--- server/modules/Roles/module.js
+++ server/modules/Roles/module.js
@@ -3,9 +3,12 @@ const Schema = mongoose.Schema;
 
 const ModuleSchema = new Schema({
   ModuleName: { type: String, required: true, unique: true },
+  Description: { type: String },
+  Order: { Type: Number },
   Path: [
     {
-      AccessType: { type: String, required: true, enum: ['VIEW', 'VIEWALL', 'VIEWDETAIL', 'UPDATE', 'DELETE', , 'ADD'] },
+      AccessType: { type: String, required: true },
+      AccessTypeDescription: { type: String },
       AdminRoutes: [{ type: String, required: true }],
       ServerRoutes: [{ route: { type: String, required: true }, method: { type: String, require: true } }],
     },
