ba9b79acf4be5342ad28c2ff04d6d7625ef00908
saileshkasaju <leosailesh@gmail.com>
2018-12-02 17:51:48 +0545

    some more translations

---
+++ client/app/containers/WtDashboard/messages.js
@@ -0,0 +1,29 @@
+/*
+ * WtDashboard Messages
+ *
+ * This contains all the text for the WtDashboard component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  dashboard: {
+    id: 'app.containers.WtDashboard.dashboard',
+    defaultMessage: 'Dashboard',
+  },
+  roleManage: {
+    id: 'app.containers.WtDashboard.roleManage',
+    defaultMessage: 'Role Manage',
+  },
+  moduleManage: {
+    id: 'app.containers.WtDashboard.moduleManage',
+    defaultMessage: 'Module Manage',
+  },
+  userManage: {
+    id: 'app.containers.WtDashboard.userManage',
+    defaultMessage: 'User Manage',
+  },
+  contentManage: {
+    id: 'app.containers.WtDashboard.contentManage',
+    defaultMessage: 'Content Manage',
+  },
+});
--- client/app/containers/WtDashboard/sidebarRoutes.js
+++ client/app/containers/WtDashboard/sidebarRoutes.js
@@ -1,3 +1,4 @@
+import React from 'react';
 // @material-ui/icons
 import People from '@material-ui/icons/People';
 import InsertChart from '@material-ui/icons/InsertChart';
@@ -5,6 +6,9 @@ import Widgets from '@material-ui/icons/Widgets';
 import PermContactCalendar from '@material-ui/icons/PermContactCalendar';
 import VPNKEY from '@material-ui/icons/VpnKey';
 import TextFormat from '@material-ui/icons/TextFormat';
+
+import { FormattedMessage } from 'react-intl';
+
 // core components/views
 import DashboardPage from '../Dashboard';
 import RoleManagePage from '../RoleManagePage';
@@ -16,36 +20,37 @@ import UserManagePage from '../UserManagePage';
 import UserManageAddEditPage from '../UserManagePage/AddEditPage';
 import ContentsListingPage from '../ContentListingPage';
 import ContentsListingAddEditPage from '../ContentListingPage/AddEditPage';
+import messages from './messages';
 
 const dashboardRoutes = [
   {
     path: '/wt/dashboard',
-    sidebarName: 'Dashboard',
-    navbarName: 'Dashboard',
+    sidebarName: <FormattedMessage {...messages.dashboard} />,
+    navbarName: <FormattedMessage {...messages.dashboard} />,
     icon: InsertChart,
     component: DashboardPage,
     exact: true,
   },
   {
     path: '/wt/role-manage/add',
-    sidebarName: 'role Manage',
-    navbarName: 'Add role',
+    sidebarName: <FormattedMessage {...messages.roleManage} />,
+    navbarName: <FormattedMessage {...messages.roleManage} />,
     icon: PermContactCalendar,
     component: RoleManageAddEditPage,
     hide: true,
   },
   {
     path: '/wt/role-manage/edit/:id',
-    sidebarName: 'role Manage',
-    navbarName: 'role Manage',
+    sidebarName: <FormattedMessage {...messages.roleManage} />,
+    navbarName: <FormattedMessage {...messages.roleManage} />,
     icon: PermContactCalendar,
     component: RoleManageAddEditPage,
     hide: true,
   },
   {
     path: '/wt/role-manage',
-    sidebarName: 'Role Manage',
-    navbarName: 'Role Manage',
+    sidebarName: <FormattedMessage {...messages.roleManage} />,
+    navbarName: <FormattedMessage {...messages.roleManage} />,
     icon: PermContactCalendar,
     component: RoleManagePage,
   },
