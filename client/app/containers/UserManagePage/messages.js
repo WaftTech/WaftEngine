1d8f0ebcf7816b3a377b0330208a36f6e1269f93
Pradip <pradipshrestha718@gmail.com>
2018-12-05 13:45:24 +0545

    edited UserManagePage

---
+++ client/app/containers/UserManagePage/messages.js
@@ -0,0 +1,33 @@
+/*
+ * UserManagePage Messages
+ *
+ * This contains all the text for the ModuleManagePage component.
+ */
+import { defineMessages } from "react-intl";
+
+export default defineMessages({
+  email: {
+    id: "app.containers.UserManagePage.email",
+    defaultMessage: "email"
+  },
+  name: {
+    id: "app.containers.UserManagePage.name",
+    defaultMessage: "name"
+  },
+  emailVerified: {
+    id: "app.containers.UserManagePage.emailVerfied",
+    defaultMessage: "emailVerfied"
+  },
+  roles: {
+    id: "app.containers.UserManagePage.roles",
+    defaultMessage: "roles"
+  },
+  avtar: {
+    id: "app.containers.UserManagePage.avtar",
+    defaultMessage: "avtar"
+  },
+  operations: {
+    id: "app.containers.UserManagePage.operations",
+    defaultMessage: "operations"
+  }
+});
