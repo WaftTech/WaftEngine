158ec9c00ee4dce5e88329f34259f882f276345a
Pujan Shrestha <shresthapujan4545@gmail.com>
2018-12-05 13:42:07 +0545

    updated role manage

---
+++ client/app/containers/RoleManagePage/messages.js
@@ -0,0 +1,25 @@
+/*
+ * ModuleManagePage Messages
+ *
+ * This contains all the text for the ModuleManagePage component.
+ */
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  roleTitle: {
+    id: 'app.containers.RoleManagePage.roleTitle',
+    defaultMessage: 'Title',
+  },
+  roleDescription: {
+    id: 'app.containers.RoleManagePage.roleDescription',
+    defaultMessage: 'Description',
+  },
+  roleIsActive: {
+    id: 'app.containers.RoleManagePage.roleIsActive',
+    defaultMessage: 'Is Active',
+  },
+  roleAction: {
+    id: 'app.containers.RoleManagePage.roleAction',
+    defaultMessage: 'Action',
+  }
+});
