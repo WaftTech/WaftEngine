8a5f29a56706290fe3a0550cd36e4be67525bb49
Manoj Kumar Mahato <mkmpvtltd@gmail.com>
2018-12-06 16:02:46 +0545

    update for modules

--- server/routes/api/roles.js
+++ server/routes/api/roles.js
@@ -4,27 +4,26 @@ const router = express.Router();
 const dModule = require('../../modules/Roles/roleController');
 const { authentication, authorization } = require('../../middleware/authentication.middleware');
 
-router.get('/role', authorization, authentication, dModule.GetRoles);
-router.get('/role/:id', authorization, authentication, dModule.GetRoleDetail);
-router.post('/role', authorization, authentication, dModule.AddRoles);
-router.get('/module', authorization, authentication, dModule.GetModule);
-router.get('/module/:id', authorization, authentication, dModule.GetModuleDetail);
-router.post('/module', authorization, authentication, dModule.AddModulList);
+router.get('/role', authorization, dModule.GetRoles);
+router.get('/role/:id', authorization, dModule.GetRoleDetail);
+router.post('/role', authorization, dModule.AddRoles);
+router.get('/module', authorization, dModule.GetModule);
+router.get('/module/:id', authorization, dModule.GetModuleDetail);
+router.post('/module', authorization, dModule.AddModulList);
 
-router.get('/access', authorization, authentication, dModule.getAccessList);
-router.post('/access', authorization, authentication, dModule.SaveAccessList);
+router.get('/access', authorization, dModule.getAccessList);
+router.post('/access', authorization, dModule.SaveAccessList);
 
 /**
  * Access Management of Role to all Module
  */
-
 router.get('/access/role/:roleid', authorization, dModule.getAccessListForRole);
-router.post('/access/role/:roleid', authorization, authentication, dModule.SaveAccessListFromRole);
+router.post('/access/role/:roleid', authorization, dModule.SaveAccessListFromRole);
+
 /**
  *Access Management of Module to all roles
  */
-
 router.get('/access/module/:moduleid', authorization, dModule.getAccessListForModule);
-router.post('/access/module/:moduleid', authorization, authentication, dModule.SaveAccessListForModule);
+router.post('/access/module/:moduleid', authorization, dModule.SaveAccessListForModule);
 
 module.exports = router;
