c7a5c7e91df7d41d9d844fa5e686eddb51a8349c
Pujan Shrestha <shresthapujan4545@gmail.com>
2018-12-13 15:22:56 +0545

    designation added ani sabai ko nepali conversion

--- client/app/containers/DesignationPage/selectors.js
+++ client/app/containers/DesignationPage/selectors.js
@@ -2,10 +2,10 @@ import { createSelector } from "reselect";
 import { initialState } from "./reducer";
 
 /**
- * Direct selector to the roleManagePage state domain
+ * Direct selector to the DesignationPage state domain
  */
 
-const selectDomain = state => state.get("blogPage", initialState);
+const selectDomain = state => state.get("designationPage", initialState);
 
 export const makeSelectAll = () =>
   createSelector(selectDomain, state => state.get("all"));
--- client/app/containers/Uchiha/tests/reducer.test.js
+++
@@ -1,8 +0,0 @@
-import { fromJS } from 'immutable';
-import uchihaReducer from '../reducer';
-
-describe('uchihaReducer', () => {
-  it('returns the initial state', () => {
-    expect(uchihaReducer(undefined, {})).toEqual(fromJS({}));
-  });
-});
