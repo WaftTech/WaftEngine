094f614b0b62bedf31b826508dc04a3683a9a61a
vikash <mevkashsah@gmail.com>
2018-12-19 13:04:11 +0545

    LeaveType Added

--- server/routes/api/LeaveType.js
+++ server/routes/api/LeaveType.js
@@ -1,13 +1,13 @@
-/*const express = require('express');
+const express = require('express');
 const router = express.Router();
 
-const LeaveTypeModule = require('../../modules/LeaveTypeController');
+const LeaveTypeModule = require('../../modules/LeaveType/LeaveTypeController');
 
 const LeaveTypeValidation = require('./../../modules/LeaveType/LeaveTypeValidation');
 
 router.get('/', LeaveTypeModule.GetLeaveType);
-router.get('/:id', LeaveType.GetLeaveTypebyID);
-router.post('/', LeaveTypeValidation.LeaveType, LeaveTypeModule.AddLeaveType);
+router.get('/:id', LeaveTypeModule.GetLeaveTypeByID);
+router.post('/', LeaveTypeValidation.validate, LeaveTypeModule.AddLeaveType);
 router.delete('/:id', LeaveTypeModule.DeleteByID);
 
 module.exports = router;
