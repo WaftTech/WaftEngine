a2268c2d0544fcaa9b9cdc32b3523f3ad152fbea
Nimesh Dahal <famous.me01@gmail.com>
2018-12-25 16:27:59 +0545

    assigned leave added

---
+++ server/modules/AssignedLeave/AssignedLeaveValidation.js
@@ -0,0 +1,120 @@
+const isEmpty = require('../../validation/isEmpty');
+const otherHelper = require('../../helper/others.helper');
+const HttpStatus = require('http-status');
+
+const { validate, sanitize } = require('./../../helper/validate.helper');
+
+const AssignedLeaveConfig = require('./AssignedLeaveConfig');
+const AssignedLeaveModel = require('./AssignedLeave');
+
+const AssignedLeavevalidation = {};
+
+AssignedLeavevalidation.Validate = async (req, res, next) => {
+  let errors = await validate(req.body, [
+    {
+      field: 'LeaveType',
+      validate: [
+        {
+          condition: 'IsEmpty',
+          msg: AssignedLeaveConfig.ValidateMessage.LeaveTypeRequired,
+        },
+        {
+          condition: 'IsMONGOID',
+          msg: AssignedLeaveConfig.ValidateMessage.LeaveTypeInvalid,
+        },
+      ],
+    },
+    {
+      field: 'FiscalYear',
+      validate: [
+        {
+          condition: 'IsEmpty',
+          msg: AssignedLeaveConfig.ValidateMessage.FiscalYearRequired,
+        },
+        {
+          condition: 'IsMONGOID',
+          msg: AssignedLeaveConfig.ValidateMessage.FiscalYearInvalid,
+        },
+      ],
+    },
+    {
+      field: 'EmployeeId',
+      validate: [
+        {
+          condition: 'IsEmpty',
+          msg: AssignedLeaveConfig.ValidateMessage.EmployeeIdRequired,
+        },
+        {
+          condition: 'IsMONGOID',
+          msg: AssignedLeaveConfig.ValidateMessage.EmployeeIdInvalid,
+        },
+      ],
+    },
+    {
+      field: 'NoOfDays',
+      validate: [
+        {
+          condition: 'IsEmpty',
+          msg: AssignedLeaveConfig.ValidateMessage.NoOfDaysRequired,
+        },
+        {
+          condition: 'IsNumeric',
+          msg: AssignedLeaveConfig.ValidateMessage.NoOfDaysInvalid,
+        },
+      ],
+    },
+    {
+      field: 'LeaveTaken',
+      validate: [
+        {
+          condition: 'IsEmpty',
+          msg: AssignedLeaveConfig.ValidateMessage.LeaveTakenRequired,
+        },
+        {
+          condition: 'IsNumeric',
+          msg: AssignedLeaveConfig.ValidateMessage.LeaveTakenInvalid,
+        },
+      ],
+    },
+    {
+      field: 'CarryOverLeave',
+      validate: [
+        {
+          condition: 'IsEmpty',
+          msg: AssignedLeaveConfig.ValidateMessage.CarryOverLeaveRequired,
+        },
+        {
+          condition: 'IsBoolean',
+          msg: AssignedLeaveConfig.ValidateMessage.CarryOverLeaveInvalid,
+        },
+      ],
+    },
+  ]);
+  console.log(errors);
+  if (!isEmpty(errors)) {
+    return otherHelper.sendResponse(res, HttpStatus.BAD_REQUEST, false, null, errors, 'Validation Error.', null);
+  } else {
+    return next();
+  }
+};
+
+AssignedLeavevalidation.Sanitize = async (req, res, next) => {
+  await sanitize(req, [
+    {
+      field: 'NoOfDays',
+      sanitize: {
+        trim: true,
+        escape: true,
+      },
+    },
+    {
+      field: 'CarryOverLeave',
+      sanitize: {
+        toBoolean: true,
+      },
+    },
+  ]);
+  next();
+};
+
+module.exports = AssignedLeavevalidation;
