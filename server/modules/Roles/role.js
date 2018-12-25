2b2589e87d9db485d6b3e8b7070cd2a39e3875a2
Manoj Kumar Mahato <mkmpvtltd@gmail.com>
2018-12-25 15:41:24 +0545

    update for client roles at sign in

--- server/modules/Roles/role.js
+++ server/modules/Roles/role.js
@@ -6,7 +6,7 @@ const RolesSchema = new Schema({
   Description: { type: String, required: false },
   IsActive: { type: Boolean, required: true, default: true },
   Added_at: { type: Date, default: Date.now },
-  Added_by: { type: Schema.Types.ObjectId },
+  Added_by: { type: Schema.Types.ObjectId, ref: 'users' },
 });
 
 module.exports = Roles = mongoose.model('roles', RolesSchema);
