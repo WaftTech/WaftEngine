f142a7bdff49822ca0920c6689d5dfef1938f386
Nimesh Dahal <famous.me01@gmail.com>
2018-12-30 22:37:54 +0545

    internal function added

--- server/routes/api/fiscal.js
+++ server/routes/api/fiscal.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const dModule = require('../../modules/fiscal/fiscalController');
+const dModule = require('../../modules/fiscal/fiscalController').FiscalController;
 const { authentication, authorization } = require('../../middleware/authentication.middleware');
 const { sanitize, validateInput } = require('../../modules/fiscal/fiscalValidation');
 
 router.get('/', authorization, dModule.GetFiscal);
 router.post('/', authorization, sanitize, validateInput, dModule.SaveFiscal);
 router.get('/:id', authorization, dModule.GetFiscalById);
-router.delete('/:id', dModule.DeleteById);
+router.delete('/:id',authorization, dModule.DeleteById);
 
 module.exports = router;
