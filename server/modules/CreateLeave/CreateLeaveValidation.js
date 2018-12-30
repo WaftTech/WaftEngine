2a74bbcdad400b3c4f67fc5a1314c8e3270e71e1
Nimesh Dahal <famous.me01@gmail.com>
2018-12-30 22:39:16 +0545

    validation optimized

--- server/modules/CreateLeave/CreateLeaveValidation.js
+++ server/modules/CreateLeave/CreateLeaveValidation.js
@@ -25,67 +25,73 @@ CreateLeavevalidation.validate = async (req, res, next) => {
     },
   ]);
 
-  for (let i = 0; i < req.body.assignedLeave.length; i++) {
-    let vdata = req.body.assignedLeave[i];
-    errors = await validate(vdata, [
-      {
-        field: 'LeaveType',
-        validate: [
-          {
-            condition: 'IsEmpty',
-            msg: CreateLeaveConfig.ValidateMessage.LeaveTypeRequired,
-          },
-          {
-            condition: 'IsMONGOID',
-            msg: CreateLeaveConfig.ValidateMessage.LeaveTypeInvalid,
-          },
-        ],
-      },
-      {
-        field: 'EmployeeId',
-        validate: [
-          {
-            condition: 'IsEmpty',
-            msg: CreateLeaveConfig.ValidateMessage.EmployeeIdRequired,
-          },
-          {
-            condition: 'IsMONGOID',
-            msg: CreateLeaveConfig.ValidateMessage.EmployeeIdInvalid,
-          },
-        ],
-      },
-      {
-        field: 'NoOfDays',
-        validate: [
-          {
-            condition: 'IsEmpty',
-            msg: CreateLeaveConfig.ValidateMessage.NoOfDaysRequired,
-          },
-          {
-            condition: 'IsNumeric',
-            msg: CreateLeaveConfig.ValidateMessage.NoOfDaysInvalid,
-          },
-        ],
-      },
-      {
-        field: 'CarryOverLeave',
-        validate: [
-          {
-            condition: 'IsEmpty',
-            msg: CreateLeaveConfig.ValidateMessage.CarryOverLeaveRequired,
-          },
-          {
-            condition: 'IsNumeric',
-            msg: CreateLeaveConfig.ValidateMessage.CarryOverLeaveInvalid,
-          },
-        ],
-      },
-    ]);
-    if (!isEmpty(errors)) {
-      errors.fieldNo = i;
-      break;
+  if (!isEmpty(req.body.assignedLeave)) {
+    for (let i = 0; i < req.body.assignedLeave.length; i++) {
+      let vdata = req.body.assignedLeave[i];
+      errors = await validate(vdata, [
+        {
+          field: 'LeaveType',
+          validate: [
+            {
+              condition: 'IsEmpty',
+              msg: CreateLeaveConfig.ValidateMessage.LeaveTypeRequired,
+            },
+            {
+              condition: 'IsMONGOID',
+              msg: CreateLeaveConfig.ValidateMessage.LeaveTypeInvalid,
+            },
+          ],
+        },
+        {
+          field: 'EmployeeId',
+          validate: [
+            {
+              condition: 'IsEmpty',
+              msg: CreateLeaveConfig.ValidateMessage.EmployeeIdRequired,
+            },
+            {
+              condition: 'IsMONGOID',
+              msg: CreateLeaveConfig.ValidateMessage.EmployeeIdInvalid,
+            },
+          ],
+        },
+        {
+          field: 'NoOfDays',
+          validate: [
+            {
+              condition: 'IsEmpty',
+              msg: CreateLeaveConfig.ValidateMessage.NoOfDaysRequired,
+            },
+            {
+              condition: 'IsNumeric',
+              msg: CreateLeaveConfig.ValidateMessage.NoOfDaysInvalid,
+            },
+          ],
+        },
+        {
+          field: 'CarryOverLeave',
+          validate: [
+            {
+              condition: 'IsEmpty',
+              msg: CreateLeaveConfig.ValidateMessage.CarryOverLeaveRequired,
+            },
+            {
+              condition: 'IsNumeric',
+              msg: CreateLeaveConfig.ValidateMessage.CarryOverLeaveInvalid,
+            },
+          ],
+        },
+      ]);
+      if (!isEmpty(errors)) {
+        errors.fieldNo = i;
+        break;
+      }
     }
+  } else {
+    let er = { LeaveType: 'No Data found!', EmployeeId: 'No Data found!', NoOfDays: 'No Data found!' };
+    return otherHelper.sendResponse(res, HttpStatus.BAD_REQUEST, false, null, er, 'No data Found!!!!', null);
   }
+
   if (!isEmpty(errors)) {
     return otherHelper.sendResponse(res, HttpStatus.BAD_REQUEST, false, null, errors, 'Validation Error.', null);
   } else {
@@ -94,26 +100,29 @@ CreateLeavevalidation.validate = async (req, res, next) => {
 };
 
 CreateLeavevalidation.sanitize = async (req, res, next) => {
-  for (let i = 0; i < req.body.assignedLeave.length; i++) {
-    let sdata = {};
-    sdata.body = req.body.assignedLeave[i];
-    await sanitize(sdata, [
-      {
-        field: 'NoOfDays',
-        sanitize: {
-          trim: true,
-          escape: true,
+  if (!isEmpty(req.body.assignedLeave)) {
+    for (let i = 0; i < req.body.assignedLeave.length; i++) {
+      let sdata = {};
+      sdata.body = req.body.assignedLeave[i];
+      await sanitize(sdata, [
+        {
+          field: 'NoOfDays',
+          sanitize: {
+            trim: true,
+            escape: true,
+          },
         },
-      },
-      {
-        field: 'CarryOverLeave',
-        sanitize: {
-          trim: true,
-          escape: true,
+        {
+          field: 'CarryOverLeave',
+          sanitize: {
+            trim: true,
+            escape: true,
+          },
         },
-      },
-    ]);
+      ]);
+    }
   }
+
   next();
 };
 
