636864d9da0638961ade0e5fccdd3c6a0e08cffb
vikash <mevkashsah@gmail.com>
2019-01-04 12:39:29 +0545

    message from controller shifted to config

--- server/modules/Branch/BranchConfig.js
+++ server/modules/Branch/BranchConfig.js
@@ -6,5 +6,10 @@ module.exports = {
     ContactNoInvalid: 'Please Enter Valid Conatct Number',
     EmailRequired: 'Please Enter Email Address',
     EmailInvalid: 'Please Enter Valid Email Address',
+
+    GetBranch: 'Branch Data delivered successfully',
+    
+    AddBranch: 'Branch Saved Success !!',
+    DeleteByID: 'Branch Data delete Success',
   },
 };
--- server/modules/Branch/BranchController.js
+++ server/modules/Branch/BranchController.js
@@ -1,6 +1,7 @@
 const HttpStatus = require('http-status');
 const otherHelper = require('../../helper/others.helper');
 const BranchModel = require('./Branch');
+const BranchConfig = require('./BranchConfig');
 const BranchController = {};
 
 BranchController.GetBranch = async (req, res, next) => {
@@ -56,14 +57,14 @@ BranchController.GetBranch = async (req, res, next) => {
 
   let datas = await otherHelper.getquerySendResponse(BranchModel, page, size, sortquery, searchquery, selectquery, next);
 
-  return otherHelper.paginationSendResponse(res, HttpStatus.OK, true, datas.data, 'Branch Data delivered successfully', page, size, datas.totaldata);
+  return otherHelper.paginationSendResponse(res, HttpStatus.OK, true, datas.data, BranchConfig.validationMessage.GetBranch, page, size, datas.totaldata);
 };
 
 BranchController.GetBranchDetail = async (req, res, next) => {
   try {
     let data = await BranchModel.findOne({ _id: req.params.id, IsDeleted: false }).select('BranchName BranchNameNepali Address ContactNo Email');
     // console.log('data:', data);
-    return otherHelper.sendResponse(res, HttpStatus.OK, true, data, null, 'Branch data delivered successfully', null);
+    return otherHelper.sendResponse(res, HttpStatus.OK, true, data, null, BranchConfig.validationMessage.GetBranch, null);
   } catch (err) {
     next(err);
   }
@@ -75,12 +76,12 @@ BranchController.AddBranch = async (req, res, next) => {
     Branch.Add_by = req.user.id;
     if (Branch._id) {
       let update = await BranchModel.findByIdAndUpdate(Branch._id, { $set: Branch });
-      return otherHelper.sendResponse(res, HttpStatus.OK, true, update, null, 'Branch Saved Success !!', null);
+      return otherHelper.sendResponse(res, HttpStatus.OK, true, update, null, BranchConfig.validationMessage.AddBranch, null);
     } else {
       // LeaveType.Added_by = req.user.id;
       let newBranch = new BranchModel(Branch);
       await newBranch.save();
-      return otherHelper.sendResponse(res, HttpStatus.OK, true, newBranch, null, 'Branch Saved Success !!', null);
+      return otherHelper.sendResponse(res, HttpStatus.OK, true, newBranch, null, BranchConfig.validationMessage.AddBranch, null);
     }
   } catch (err) {
     next(err);
@@ -91,7 +92,7 @@ BranchController.DeleteByID = async (req, res, next) => {
   try {
     const id = req.params.id;
     const data = await BranchModel.findByIdAndUpdate(id, { $set: { IsDeleted: true, Deleted_By: req.user.id, Deleted_At: new Date() } });
-    return otherHelper.sendResponse(res, HttpStatus.OK, true, data, null, 'Branch Data delete Success', null);
+    return otherHelper.sendResponse(res, HttpStatus.OK, true, data, null, BranchConfig.validationMessage.DeleteByID, null);
   } catch (err) {
     next(err);
   }
