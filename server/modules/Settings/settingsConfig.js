2a4166b29277e8368ba39c82d3ceb32782d2a99a
Nimesh Dahal <famous.me01@gmail.com>
2019-01-04 12:59:37 +0545

    settings added

---
+++ server/modules/Settings/settingsConfig.js
@@ -0,0 +1,6 @@
+module.exports = {
+  validationMessage: {
+    keyRequired: 'I think you missed the Key field!!',
+    keyInvalidLength: 'Sorry, Key must be atleast 2 characters and maximum 50 characters!',
+  },
+};
