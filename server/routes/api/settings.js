363cbbb090b34f8cd42d0879aa6dab95b3889bb1
Nimesh Dahal <famous.me01@gmail.com>
2019-01-04 12:59:51 +0545

    settings added

---
+++ server/routes/api/settings.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+const settingsModule = require('./../../modules/Settings/settingsController').settingsController;
+
+const { authorization } = require('../../middleware/authentication.middleware');
+
+const settingsValidation = require('./../../modules/settings/settingsValidation');
+
+router.get('/', authorization, settingsModule.getData);
+router.get('/:key', authorization, settingsModule.getDataByKey);
+router.post('/', authorization,settingsValidation.sanitize, settingsValidation.validate, settingsModule.saveData);
+
+module.exports = router;
