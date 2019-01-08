ced9d8779bc5d6dbb03deccf3a78b2949ea34b6a
Nimesh Dahal <famous.me01@gmail.com>
2019-01-08 16:37:34 +0545

    isactive default true

--- server/modules/Roles/access.js
+++ server/modules/Roles/access.js
@@ -5,7 +5,7 @@ const AccessSchema = new Schema({
   ModuleId: { type: Schema.Types.ObjectId, required: true, ref: 'moduleAccess' },
   RoleId: { type: Schema.Types.ObjectId, required: true, ref: 'roles' },
   AccessType: { type: [Schema.Types.ObjectId], required: true },
-  IsActive: { type: Boolean, required: true, default: false },
+  IsActive: { type: Boolean, required: true, default: true },
   Added_at: { type: Date, default: Date.now },
 });
 
