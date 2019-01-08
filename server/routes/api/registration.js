0a4883764dfa369f5e38ab91e435cbe532f76712
Nimesh Dahal <famous.me01@gmail.com>
2019-01-08 16:42:26 +0545

    authentication added

--- server/routes/api/registration.js
+++ server/routes/api/registration.js
@@ -1,17 +1,17 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const upload = multer({ dest: 'public/registration/' });
 
 const registrationModule = require('./../../modules/registration/registrationController');
 
-const { authorization } = require('../../middleware/authentication.middleware');
+const { authorization, authentication } = require('../../middleware/authentication.middleware');
 
 const registrationValidation = require('./../../modules/registration/registrationValidation');
 
-router.get('/', authorization, registrationModule.getData);
-router.get('/:id', authorization, registrationModule.getDataByID);
-router.post('/', authorization, upload.array('file', 1), registrationValidation.sanitize, registrationValidation.validate, registrationValidation.duplicateValidation, registrationModule.saveData);
-router.delete('/:id', authorization, registrationModule.deleteById);
+router.get('/', authorization, authentication, registrationModule.getData);
+router.get('/:id', authorization, authentication, registrationModule.getDataByID);
+router.post('/', authorization, authentication, upload.array('file', 1), registrationValidation.sanitize, registrationValidation.validate, registrationValidation.duplicateValidation, registrationModule.saveData);
+router.delete('/:id', authorization, authentication, registrationModule.deleteById);
 
 module.exports = router;
