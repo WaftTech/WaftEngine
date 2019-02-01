6bd48d6b356d8d6b78e01fc55e5f20fc9027288f
Navin Mishra <navinmishra1717@gmail.com>
2019-02-01 18:31:44 +0545

    data insert added

--- server/dumpData/constants.js
+++ server/dumpData/constants.js
@@ -2,7 +2,7 @@ module.exports = {
   userData: [{ name: 'rajan', email: 'rajan@gmail.com', password: 'rajan123', password: 'rajan123', gender: 'male' }, { name: 'manoj', email: 'manoj@gmail.com', password: 'manoj123', password: 'manoj123', gender: 'male' }],
   roleData: {},
   contentData: {},
-  blogData: {},
+  blogData: [{ title: 'rajan daju', description: 'its very challenging!!' }, { title: 'navin', description: 'my name is navin mishra' }],
   moduleData: {},
   mediaData: {},
 };
