28665ca0aa34315072a68608ecc687cc5e1f80c5
Navin Mishra <navinmishra1717@gmail.com>
2019-02-06 18:05:03 +0545

    faq added

---
+++ server/modules/faq/faqValidation.js
@@ -0,0 +1,41 @@
+const httpStatus = require('http-status');
+const isEmpty = require('../../validation/isEmpty');
+const otherHelper = require('../../helper/others.helper');
+const faqConfig = require('./faqConfig');
+const faqValidation = {};
+faqValidation.Sanitize = (req, res, next) => {
+  const sanitizeArray = [
+    {
+      field: 'title',
+      sanitize: {
+        trim: true,
+      },
+    },
+  ];
+  otherHelper.sanitize(req, sanitizeArray);
+  next();
+};
+faqValidation.Validation = (req, res, next) => {
+  const validateArray = [
+    {
+      field: 'title',
+      validate: [
+        {
+          condition: 'IsEmpty',
+          msg: faqConfig.validate.isEmpty,
+        },
+        {
+          condition: 'IsLength',
+          msg: faqConfig.validate.isLength,
+        },
+      ],
+    },
+  ];
+  const errors = otherHelper.validation(req.body, validateArray);
+  if (!isEmpty(errors)) {
+    return otherHelper.sendResponse(res, httpStatus.BAD_REQUEST, false, null, errors, 'invalid input', null);
+  } else {
+    next();
+  }
+};
+module.exports = faqValidation;
