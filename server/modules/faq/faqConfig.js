c5c29062dc8f92068c132f2b9ca4fe4b2ff44ea3
Navin Mishra <navinmishra1717@gmail.com>
2019-02-06 18:05:03 +0545

    faq added

---
+++ server/modules/faq/faqConfig.js
@@ -0,0 +1,15 @@
+module.exports = {
+  validate: {
+    isEmpty: 'This field is required!!',
+    isLength: 'This field length should be between 2 to 2000!!',
+    catLength: 'Category length should be between 2 to 500!!',
+  },
+  faqSave: 'Faq save successful!!',
+  faqGet: 'Faq get successful!!',
+  ansSave: 'Faq Answer save successful!!',
+  ansGet: 'Faq Answer get successful!!',
+  catSave: 'Faq category save successful!!',
+  catGet: 'Faq category get successful!!',
+  ansDelete: 'Faq answer deleted successfully!!',
+  faqDelete: 'faq deleted successfully!!',
+};
