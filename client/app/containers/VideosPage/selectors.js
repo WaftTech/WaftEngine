28c5050f583eb833fff39a46a5455bbddd219c99
Navin Mishra <navinmishra1717@gmail.com>
2019-02-19 17:58:25 +0545

    video url adding

--- client/app/containers/VideosPage/selectors.js
+++ client/app/containers/VideosPage/selectors.js
@@ -1,6 +1,6 @@
 import { createSelector } from 'reselect';
 import { initialState } from './reducer';
 
-const selectDomain = state => state.get('videos', initialState);
+const selectDomain = state => state.get('videoPage', initialState);
 
 export const makeSelectVideos = () => createSelector(selectDomain, state => state.get('videos'));
