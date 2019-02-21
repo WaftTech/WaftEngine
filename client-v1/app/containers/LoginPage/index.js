2b6948630888448c0004d23a258fc2f21cd04e6b
saileshkasaju <leosailesh@gmail.com>
2019-02-21 18:47:00 +0545

    style login page

--- client-v1/app/containers/LoginPage/index.js
+++ client-v1/app/containers/LoginPage/index.js
@@ -1,44 +1,52 @@
-/**
- *
- * LoginPage
- *
- */
-
 import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
-
 // @material-ui/core components
 import withStyles from '@material-ui/core/styles/withStyles';
-import image from 'assets/img/bg7.jpg';
-
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Icon from '@material-ui/core/Icon';
+// @material-ui/icons
+import Email from '@material-ui/icons/Email';
+import People from '@material-ui/icons/People';
+// core components
+import Header from 'components/Header/Header';
+import HeaderLinks from 'components/Header/HeaderLinks';
 import Footer from 'components/Footer/Footer';
-import Card from 'components/Card/Card';
 import GridContainer from 'components/Grid/GridContainer';
 import GridItem from 'components/Grid/GridItem';
-import injectSaga from 'utils/injectSaga';
-import injectReducer from 'utils/injectReducer';
-import reducer from './reducer';
-import saga from './saga';
-import * as mapDispatchToProps from './actions';
-import UsernameInput from './components/UsernameInput';
-import PasswordInput from './components/PasswordInput';
-import styles from './styles';
+import Button from 'components/CustomButtons/Button';
+import Card from 'components/Card/Card';
+import CardBody from 'components/Card/CardBody';
+import CardHeader from 'components/Card/CardHeader';
+import CardFooter from 'components/Card/CardFooter';
+import CustomInput from 'components/CustomInput/CustomInput';
 
-class LoginPage extends React.PureComponent {
-  state = { cardAnimaton: 'cardHidden' };
+import image from 'assets/img/bg7.jpg';
+import loginPageStyle from './styles';
+
+class LoginPage extends React.Component {
+  constructor(props) {
+    super(props);
+    // we use this to make the card to appear after the page has been rendered
+    this.state = {
+      cardAnimaton: 'cardHidden',
+    };
+  }
 
   componentDidMount() {
-    this.timer = setTimeout(() => this.setState({ cardAnimaton: '' }), 700);
+    // we add a hidden class to the card and after 700 ms we delete it and the transition appears
+    setTimeout(() => this.setState({ cardAnimaton: '' }), 700);
   }
 
   render() {
-    const { classes } = this.props;
+    const { classes, ...rest } = this.props;
     return (
       <>
-        {/* <Header /> */}
-
+        <Header
+          absolute
+          color='transparent'
+          brand='Waft Engine'
+          rightLinks={<HeaderLinks />}
+          {...rest}
+        />
         <div
           className={classes.pageHeader}
           style={{
@@ -48,13 +56,97 @@ class LoginPage extends React.PureComponent {
           }}
         >
           <div className={classes.container}>
-            <GridContainer justify="center">
+            <GridContainer justify='center'>
               <GridItem xs={12} sm={12} md={4}>
                 <Card className={classes[this.state.cardAnimaton]}>
                   <form className={classes.form}>
-                    <UsernameInput />
-                    <PasswordInput />
-                    <button type="submit">Submit</button>
+                    <CardHeader color='primary' className={classes.cardHeader}>
+                      <h4>Login</h4>
+                      <div className={classes.socialLine}>
+                        <Button
+                          justIcon
+                          href='#pablo'
+                          target='_blank'
+                          color='transparent'
+                          onClick={e => e.preventDefault()}
+                        >
+                          <i className={'fab fa-twitter'} />
+                        </Button>
+                        <Button
+                          justIcon
+                          href='#pablo'
+                          target='_blank'
+                          color='transparent'
+                          onClick={e => e.preventDefault()}
+                        >
+                          <i className={'fab fa-facebook'} />
+                        </Button>
+                        <Button
+                          justIcon
+                          href='#pablo'
+                          target='_blank'
+                          color='transparent'
+                          onClick={e => e.preventDefault()}
+                        >
+                          <i className={'fab fa-google-plus-g'} />
+                        </Button>
+                      </div>
+                    </CardHeader>
+                    <p className={classes.divider}>Or Be Classical</p>
+                    <CardBody>
+                      <CustomInput
+                        labelText='First Name...'
+                        id='first'
+                        formControlProps={{
+                          fullWidth: true
+                        }}
+                        inputProps={{
+                          type: 'text',
+                          endAdornment: (
+                            <InputAdornment position='end'>
+                              <People className={classes.inputIconsColor} />
+                            </InputAdornment>
+                          )
+                        }}
+                      />
+                      <CustomInput
+                        labelText='Email...'
+                        id='email'
+                        formControlProps={{
+                          fullWidth: true
+                        }}
+                        inputProps={{
+                          type: 'email',
+                          endAdornment: (
+                            <InputAdornment position='end'>
+                              <Email className={classes.inputIconsColor} />
+                            </InputAdornment>
+                          )
+                        }}
+                      />
+                      <CustomInput
+                        labelText='Password'
+                        id='pass'
+                        formControlProps={{
+                          fullWidth: true
+                        }}
+                        inputProps={{
+                          type: 'password',
+                          endAdornment: (
+                            <InputAdornment position='end'>
+                              <Icon className={classes.inputIconsColor}>
+                                lock_outline
+                              </Icon>
+                            </InputAdornment>
+                          )
+                        }}
+                      />
+                    </CardBody>
+                    <CardFooter className={classes.cardFooter}>
+                      <Button simple color='primary' size='lg'>
+                        Get started
+                      </Button>
+                    </CardFooter>
                   </form>
                 </Card>
               </GridItem>
@@ -67,25 +159,4 @@ class LoginPage extends React.PureComponent {
   }
 }
 
-LoginPage.propTypes = {
-  classes: PropTypes.object,
-};
-
-const mapStateToProps = null;
-
-const withConnect = connect(
-  mapStateToProps,
-  mapDispatchToProps,
-);
-
-const withReducer = injectReducer({ key: 'loginPage', reducer });
-const withSaga = injectSaga({ key: 'loginPage', saga });
-
-const withStyle = withStyles(styles);
-
-export default compose(
-  withStyle,
-  withReducer,
-  withSaga,
-  withConnect,
-)(LoginPage);
+export default withStyles(loginPageStyle)(LoginPage);
