244ab265a6971ee33006185925d3c9ba2208e335
Navin Mishra <navinmishra1717@gmail.com>
2019-02-22 13:38:02 +0545

    signup in progress

--- client-v1/app/containers/SignupPage/components/UsernameInput.js
+++ client-v1/app/containers/SignupPage/components/UsernameInput.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import CustomInput from 'components/CustomInput/CustomInput';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Icon from '@material-ui/icons/People';
 import { createStructuredSelector } from 'reselect';
 import { makeSelectUsername } from '../selectors';
 import * as mapDispatchToProps from '../actions';
 
 const UsernameInput = props => {
-  const { username, setStoreValue } = props; // eslint-disable-line
+  const { username, setStoreValue, classes } = props; // eslint-disable-line
   return (
     <CustomInput
       labelText="username"
@@ -14,7 +16,15 @@ const UsernameInput = props => {
       formControlProps={{
         fullWidth: true,
       }}
-      inputProps={{ value: username, onChange: e => setStoreValue({ key: 'username', value: e.target.value }) }}
+      inputProps={{
+        value: username,
+        onChange: e => setStoreValue({ key: 'username', value: e.target.value }),
+        endAdornment: (
+          <InputAdornment position="end">
+            <Icon className={classes.inputIconsColor} />
+          </InputAdornment>
+        ),
+      }}
     />
   );
 };
