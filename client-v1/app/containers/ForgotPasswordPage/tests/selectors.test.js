97148733a94eb0f0d827729721ab82b473eef6f1
Navin Mishra <navinmishra1717@gmail.com>
2019-02-27 12:52:02 +0545

    forgotpassword added

---
+++ client-v1/app/containers/ForgotPasswordPage/tests/selectors.test.js
@@ -0,0 +1,8 @@
+// import { fromJS } from 'immutable';
+// import { selectForgotPasswordPageDomain } from '../selectors';
+
+describe('selectForgotPasswordPageDomain', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
