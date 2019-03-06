2a39b52b4230aeaea5143f9f5c8d5913f512e5cf
saileshkasaju <leosailesh@gmail.com>
2019-03-06 17:34:27 +0545

    minor fixes

--- client-v1/app/app.js
+++ client-v1/app/app.js
@@ -23,6 +23,7 @@ import { setUser, setToken } from 'containers/App/actions';
 // Import Language Provider
 import LanguageProvider from 'containers/LanguageProvider';
 
+import 'assets/css/material-dashboard-react.css?v=1.6.0';
 // Load the favicon and the .htaccess file
 /* eslint-disable import/no-unresolved, import/extensions */
 import '!file-loader?name=[name].[ext]!./images/favicon.ico';
@@ -33,8 +34,6 @@ import configureStore from './configureStore';
 
 // Import i18n messages
 import { translationMessages } from './i18n';
-
-import 'moment/locale/en-gb';
 
 // Create redux store with history
 const initialState = {};
