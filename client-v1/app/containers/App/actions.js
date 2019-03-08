4ba979e9f63dc41e894bbc197e52122fef530e28
Navin Mishra <navinmishra1717@gmail.com>
2019-03-08 16:49:02 +0545

    redirect done

--- client-v1/app/containers/App/actions.js
+++ client-v1/app/containers/App/actions.js
@@ -3,7 +3,7 @@ import * as types from './constants';
 /**
  * sets the user
  *
- * @param  {object} user The userInfo
+ * @param  {object} payload The userInfo
  *
  * @return {object}       An action object with a type of SET_USER passing the user
  */
@@ -12,7 +12,7 @@ export const setUser = payload => ({ type: types.SET_USER, payload });
 /**
  * sets the token
  *
- * @param  {string} user The Authentication token
+ * @param  {string} payload The Authentication token
  *
  * @return {object}       An action object with a type of SET_TOKEN passing the token
  */
@@ -25,10 +25,17 @@ export const setToken = payload => ({ type: types.SET_TOKEN, payload });
  */
 export const logout = () => ({ type: types.LOGOUT });
 
+/**
+ *
+ * @param {string} payload The email id of subscriber
+ *
+ * @return {object}       An action object with a type of SUBCRIBE_REQUEST passing the email
+ */
 export const subscribeRequest = payload => ({
   type: types.SUBSCRIBE_REQUEST,
   payload,
 });
+
 export const subscribeSuccess = payload => ({
   type: types.SUBSCRIBE_SUCCESS,
   payload,
