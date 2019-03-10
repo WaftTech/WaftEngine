bfd9e6cc1d50162d352b1aa264abc00ba8a9d297
Navin Mishra <navinmishra1717@gmail.com>
2019-03-10 17:34:45 +0545

    setup in proooooogress

--- client-v1/app/routes/guest.js
+++ client-v1/app/routes/guest.js
@@ -1,5 +1,8 @@
 import LoginPage from '../containers/LoginPage/Loadable';
 import AdminLoginPage from '../containers/AdminLoginPage/Loadable';
+import SignupPage from '../containers/SignupPage';
+import ForgotPasswordPage from '../containers/ForgotPasswordPage';
+import CodeVerifyPage from '../containers/CodeVerifyPage';
 
 const routes = [
   {
@@ -10,6 +13,21 @@ const routes = [
   {
     path: '/auth/admin-login',
     component: AdminLoginPage,
+    exact: true,
+  },
+  {
+    path: '/auth/signup',
+    component: SignupPage,
+    exact: true,
+  },
+  {
+    path: '/auth/reset-password',
+    component: ForgotPasswordPage,
+    exact: true,
+  },
+  {
+    path: '/auth/reset-password/code-verify',
+    component: CodeVerifyPage,
     exact: true,
   },
 ];
