2594ad98a263a2fb3b36645777653146d8958aa7
Navin Mishra <navinmishra1717@gmail.com>
2019-03-18 18:11:40 +0545

    public routes managed

--- client-v1/app/routes/public.js
+++ client-v1/app/routes/public.js
@@ -1,5 +1,13 @@
 import HomePage from '../containers/HomePage';
-import BlogPage from '../containers/BlogList';
+import BlogList from '../containers/BlogList';
+import BlogPage from '../containers/BlogPage';
+import ContactUs from '../containers/ContactUs';
+import AboutUs from '../containers/AboutUs';
+import Videos from '../containers/VideoLibraryListingPage';
+import VideosPage from '../containers/VideosPage';
+import CategoryDetailPage from '../containers/CategoryDetailPage';
+import CategoryListingPage from '../containers/CategoryListingPage';
+import VideoDetailPage from '../containers/VideoDetailPage';
 
 const publicRoutes = [
   {
@@ -10,7 +18,49 @@ const publicRoutes = [
   {
     exact: true,
     path: '/blog-list',
+    component: BlogList,
+  },
+  {
+    exact: true,
+    path: '/blog/:slug',
     component: BlogPage,
+  },
+
+  {
+    exact: true,
+    path: '/blog-category',
+    component: CategoryListingPage,
+  },
+  {
+    exact: true,
+    path: '/blog-category/:id',
+    component: CategoryDetailPage,
+  },
+
+  {
+    exact: true,
+    path: '/contact-us',
+    component: ContactUs,
+  },
+  {
+    exact: true,
+    path: '/about-us',
+    component: AboutUs,
+  },
+  {
+    exact: true,
+    path: '/videos',
+    component: Videos,
+  },
+  {
+    exact: true,
+    path: '/video/:id',
+    component: VideosPage,
+  },
+  {
+    exact: true,
+    path: '/video/link/:id',
+    component: VideoDetailPage,
   },
 ];
 
