afe02632120402e397fd1a9a091c72f3bf70081b
Navin Mishra <navinmishra1717@gmail.com>
2019-03-26 17:09:54 +0545

    issue solved

--- server/modules/content/contentController.js
+++ server/modules/content/contentController.js
@@ -7,55 +7,59 @@ const contentController = {};
 const internal = {};
 
 contentController.GetContent = async (req, res, next) => {
-  const size_default = 10;
-  let page;
-  let size;
-  let searchq;
-  let sortq;
-  let selectq;
-  if (req.query.page && !isNaN(req.query.page) && req.query.page != 0) {
-    page = Math.abs(req.query.page);
-  } else {
-    page = 1;
-  }
-  if (req.query.size && !isNaN(req.query.size) && req.query.size != 0) {
-    size = Math.abs(req.query.size);
-  } else {
-    size = size_default;
-  }
-  if (req.query.sort) {
-    let sortfield = req.query.sort.slice(1);
-    let sortby = req.query.sort.charAt(0);
-    console.log(sortfield);
-    if (sortby == 1 && !isNaN(sortby) && sortfield) {
-      //one is ascending
-      sortq = sortfield;
-    } else if (sortby == 0 && !isNaN(sortby) && sortfield) {
-      //zero is descending
-      sortq = '-' + sortfield;
+  try {
+    const size_default = 10;
+    let page;
+    let size;
+    let searchq;
+    let sortq;
+    let selectq;
+    if (req.query.page && !isNaN(req.query.page) && req.query.page != 0) {
+      page = Math.abs(req.query.page);
     } else {
-      sortq = '';
+      page = 1;
     }
-  }
+    if (req.query.size && !isNaN(req.query.size) && req.query.size != 0) {
+      size = Math.abs(req.query.size);
+    } else {
+      size = size_default;
+    }
+    if (req.query.sort) {
+      let sortfield = req.query.sort.slice(1);
+      let sortby = req.query.sort.charAt(0);
+      console.log(sortfield);
+      if (sortby == 1 && !isNaN(sortby) && sortfield) {
+        //one is ascending
+        sortq = sortfield;
+      } else if (sortby == 0 && !isNaN(sortby) && sortfield) {
+        //zero is descending
+        sortq = '-' + sortfield;
+      } else {
+        sortq = '';
+      }
+    }
 
-  searchq = { is_deleted: false };
+    searchq = { is_deleted: false };
 
-  if (req.query.find_name) {
-    searchq = { name: { $regex: req.query.find_name, $options: 'i x' }, ...searchq };
-  }
-  if (req.query.find_key) {
-    searchq = { key: { $regex: req.query.find_key, $options: 'i x' }, ...searchq };
-  }
-  if (req.query.find_publish_from) {
-    searchq = { publish_from: { $regex: req.query.find_publish_from, $options: 'i x' }, ...searchq };
-  }
-  if (req.query.find_publish_to) {
-    searchq = { publish_to: { $regex: req.query.find_publish_to, $options: 'i x' }, ...searchq };
-  }
-  selectq = 'name key description publish_from publish_to is_active is_feature';
-  let datas = await otherHelper.getquerySendResponse(contentSch, page, size, sortq, searchq, selectq, next, '');
+    if (req.query.find_name) {
+      searchq = { name: { $regex: req.query.find_name, $options: 'i x' }, ...searchq };
+    }
+    if (req.query.find_key) {
+      searchq = { key: { $regex: req.query.find_key, $options: 'i x' }, ...searchq };
+    }
+    if (req.query.find_publish_from) {
+      searchq = { publish_from: { $regex: req.query.find_publish_from, $options: 'i x' }, ...searchq };
+    }
+    if (req.query.find_publish_to) {
+      searchq = { publish_to: { $regex: req.query.find_publish_to, $options: 'i x' }, ...searchq };
+    }
+    selectq = 'name key description publish_from publish_to is_active is_feature';
+    let datas = await otherHelper.getquerySendResponse(contentSch, page, size, sortq, searchq, selectq, next, '');
 
-  return otherHelper.paginationSendResponse(res, httpStatus.OK, true, datas.data, contentConfig.gets, page, size, datas.totaldata);
+    return otherHelper.paginationSendResponse(res, httpStatus.OK, true, datas.data, contentConfig.gets, page, size, datas.totaldata);
+  } catch (err) {
+    next(err);
+  }
 };
 contentController.SaveContent = async (req, res, next) => {
   try {
@@ -74,14 +78,22 @@ contentController.SaveContent = async (req, res, next) => {
   }
 };
 contentController.GetContentDetail = async (req, res, next) => {
-  const id = req.params.id;
-  const contents = await contentSch.findOne({ _id: id, is_deleted: false });
-  return otherHelper.sendResponse(res, httpStatus.OK, true, contents, null, contentConfig.get, null);
+  try {
+    const id = req.params.id;
+    const contents = await contentSch.findOne({ _id: id, is_deleted: false });
+    return otherHelper.sendResponse(res, httpStatus.OK, true, contents, null, contentConfig.get, null);
+  } catch (err) {
+    next(err);
+  }
 };
 contentController.GetContentByKey = async (req, res, next) => {
-  const key = req.params.key;
-  const contents = await contentSch.findOne({ key, is_deleted: false });
-  return otherHelper.sendResponse(res, httpStatus.OK, true, contents, null, contentConfig.get, null);
+  try {
+    const key = req.params.key;
+    const contents = await contentSch.findOne({ key, is_deleted: false });
+    return otherHelper.sendResponse(res, httpStatus.OK, true, contents, null, contentConfig.get, null);
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports = contentController;
