1a53dfa6bb3e5a2b8d02f4abc673e52ee2b22726
saileshkasaju <leosailesh@gmail.com>
2019-03-28 19:04:22 +0545

    snackbar setup test

--- client-v2/app/containers/App/index.js
+++ client-v2/app/containers/App/index.js
@@ -7,8 +7,11 @@
  */
 
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Switch, Route } from 'react-router-dom';
 import { compose } from 'redux';
+import { connect } from 'react-redux';
+import Button from '@material-ui/core/Button';
 
 import injectSaga from 'utils/injectSaga';
 
@@ -20,18 +23,46 @@ import RoutesUser from '../../layouts/User';
 import GlobalStyle from '../../global-styles';
 import AdminRoute from '../../components/Routes/AdminRoute';
 import UserRoute from '../../components/Routes/UserRoute';
+import Notifier from './components/Notifier';
+import { enqueueSnackbar } from './actions';
 
-const App = () => (
-  <>
-    <Switch>
-      <UserRoute path="/user" component={RoutesUser} />
-      <AdminRoute path="/admin" component={RoutesAdmin} />
-      <Route path="/" component={RoutesPublic} />
-    </Switch>
-    <GlobalStyle />
-  </>
-);
+const App = props => {
+  const handleClick = () => {
+    props.enqueueSnackbar({
+      message: 'Failed fetching data.',
+      options: {
+        variant: 'warning',
+      },
+    });
+  };
+  return (
+    <>
+      <Button variant="contained" onClick={handleClick}>
+        Display snackbar
+      </Button>
+      <Notifier />
+      <Switch>
+        <UserRoute path="/user" component={RoutesUser} />
+        <AdminRoute path="/admin" component={RoutesAdmin} />
+        <Route path="/" component={RoutesPublic} />
+      </Switch>
+      <GlobalStyle />
+    </>
+  );
+};
+
+App.propTypes = {
+  enqueueSnackbar: PropTypes.func.isRequired,
+};
 
 const withSaga = injectSaga({ key: 'global', saga });
 
-export default compose(withSaga)(App);
+const withConnect = connect(
+  null,
+  { enqueueSnackbar },
+);
+
+export default compose(
+  withSaga,
+  withConnect,
+)(App);
