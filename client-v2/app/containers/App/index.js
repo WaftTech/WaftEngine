b8dd66e56fdcb0e87726e8d2c37940253f442736
saileshkasaju <leosailesh@gmail.com>
2019-03-29 12:12:37 +0545

    setup notification and add example in admin user manage saga

--- client-v2/app/containers/App/index.js
+++ client-v2/app/containers/App/index.js
@@ -7,11 +7,9 @@
  */
 
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Switch, Route } from 'react-router-dom';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
-import Button from '@material-ui/core/Button';
 
 import injectSaga from 'utils/injectSaga';
 
@@ -26,34 +24,17 @@ import UserRoute from '../../components/Routes/UserRoute';
 import Notifier from './components/Notifier';
 import { enqueueSnackbar } from './actions';
 
-const App = props => {
-  const handleClick = () => {
-    props.enqueueSnackbar({
-      message: 'Failed fetching data.',
-      options: {
-        variant: 'warning',
-      },
-    });
-  };
-  return (
-    <>
-      <Button variant="contained" onClick={handleClick}>
-        Display snackbar
-      </Button>
-      <Notifier />
-      <Switch>
-        <UserRoute path="/user" component={RoutesUser} />
-        <AdminRoute path="/admin" component={RoutesAdmin} />
-        <Route path="/" component={RoutesPublic} />
-      </Switch>
-      <GlobalStyle />
-    </>
-  );
-};
-
-App.propTypes = {
-  enqueueSnackbar: PropTypes.func.isRequired,
-};
+const App = () => (
+  <>
+    <Notifier />
+    <Switch>
+      <UserRoute path="/user" component={RoutesUser} />
+      <AdminRoute path="/admin" component={RoutesAdmin} />
+      <Route path="/" component={RoutesPublic} />
+    </Switch>
+    <GlobalStyle />
+  </>
+);
 
 const withSaga = injectSaga({ key: 'global', saga });
 
