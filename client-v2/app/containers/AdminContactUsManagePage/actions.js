0168353aeb3fe691e296d054a54228c009ff7068
Navin Mishra <navinmishra1717@gmail.com>
2019-04-08 13:34:49 +0545

    contact us added

---
+++ client-v2/app/containers/AdminContactUsManagePage/actions.js
@@ -0,0 +1,13 @@
+/*
+ *
+ * AdminContactUsManagePage actions
+ *
+ */
+
+import { DEFAULT_ACTION } from './constants';
+
+export function defaultAction() {
+  return {
+    type: DEFAULT_ACTION,
+  };
+}
---
+++ client-v2/app/containers/AdminContactUsManagePage/messages.js
@@ -0,0 +1,16 @@
+/*
+ * AdminContactUsManagePage Messages
+ *
+ * This contains all the text for the AdminContactUsManagePage container.
+ */
+
+import { defineMessages } from 'react-intl';
+
+export const scope = 'app.containers.AdminContactUsManagePage';
+
+export default defineMessages({
+  header: {
+    id: `${scope}.header`,
+    defaultMessage: 'This is the AdminContactUsManagePage container!',
+  },
+});
