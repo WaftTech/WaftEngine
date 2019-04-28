615962837cdfe64aa66259c31e8b0fc3ff62c0a6
jivanshr <jivanshr@gmail.com>
2019-04-28 12:32:22 +0545

    home page content deleted

--- client/app/containers/AboutUsPage/saga.js
+++
@@ -1,6 +0,0 @@
-// import { take, call, put, select } from 'redux-saga/effects';
-
-// Individual exports for testing
-export default function* aboutUsPageSaga() {
-  // See example in containers/HomePage/saga.js
-}
