864aceef4e3f12f36073c8aea4f68d423bb211f1
jivanshr <jivanshr@gmail.com>
2019-04-28 12:32:22 +0545

    home page content deleted

--- client/app/layouts/components/Header.js
+++ client/app/layouts/components/Header.js
@@ -5,70 +5,16 @@ import { NavLink, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { push } from 'connected-react-router';
-// import {Helmet} from "react-helmet";
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-// import logo from 'images/hello-tender-1.png';
+import { Button, Grid, Menu, MenuItem } from '@material-ui/core';
 import { createStructuredSelector } from 'reselect';
 import {
   makeSelectToken,
   makeSelectUser,
 } from '../../containers/App/selectors';
 import { logoutRequest } from '../../containers/App/actions';
-import logo from '../../assets/img/we_logo.png';
 
-const styles = theme => ({
-  logo20: {
-    width: 200,
-    margin: 10,
-  },
-  button: {
-    margin: theme.spacing.unit,
-    justifyContent: 'flex-end',
-    marginTop: 20,
-    marginBottom: 20,
-    boxShadow: 'none',
-    '&:hover': {
-      background: '#cf0b24',
-      color: '#fff',
-    },
-  },
-  topRightWrapper: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-End',
-    height: '100%',
-  },
-  menu: {
-    marginRight: 30,
-    marginLeft: 30,
-    color: '#fff',
-    textDecoration: 'none',
-    fontSize: 18,
-  },
-  menuWrapper: {
-    background: '#cf0b24',
-    width: '100%',
-    '& a': {
-      display: 'inline-block',
-      padding: '15px',
-      fontSize: 14,
-      margin: 0,
-    },
-    '& a:hover': {
-      backgroundColor: 'rgba(0,0,0,0.2)',
-    },
-    '& a.active': {
-      boxShadow: 'inset 0 -4px 0 rgba(0,0,0,0.8)',
-    },
-  },
-  dropDown: {
-    textTransform: 'capitalize',
-  },
-});
+const styles = theme => ({});
 
 const Header = props => {
   const { classes, token, user, logoutRequest: logout } = props;
@@ -94,105 +40,70 @@ const Header = props => {
   };
 
   return (
-    <div>
-      <header>
-        <div className="container">
-          <Grid container>
-            <Grid item xs={6} sm={6}>
-              <img src={logo} alt="Waft Engine" className={classes.logo20} />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              {!token ? (
-                <div className={classes.topRightWrapper}>
-                  <Button
-                    onClick={redirectToRegister}
-                    variant="contained"
-                    className={classes.button}
-                  >
-                    Register
-                  </Button>
-                  <Button
-                    onClick={redirectToLogin}
-                    variant="contained"
-                    className={classes.button}
-                  >
-                    Login
-                  </Button>
-                </div>
-              ) : (
-                <div className={classes.topRightWrapper}>
-                  <Button className={classes.dropDown} onClick={handleMenu}>
-                    <span>{user.name}</span>
-
-                    <AccountCircle />
-                  </Button>
-                  <Menu
-                    id="menu-appbar"
-                    anchorEl={anchorEl}
-                    anchorOrigin={{
-                      vertical: 'top',
-                      horizontal: 'right',
-                    }}
-                    transformOrigin={{
-                      vertical: 'top',
-                      horizontal: 'right',
-                    }}
-                    open={open}
-                    onClose={handleClose}
-                  >
-                    {user.isAdmin && (
-                      <MenuItem onClick={handleClose}>
-                        <Link
-                          to="/admin/dashboard"
-                          style={{ textDecoration: 'none', color: 'black' }}
-                        >
-                          Dashboard
-                        </Link>
-                      </MenuItem>
-                    )}
-                    <MenuItem onClick={handleClose}>
-                      <Link
-                        to="/user/profile"
-                        style={{ textDecoration: 'none', color: 'black' }}
-                      >
-                        Profile
-                      </Link>
-                    </MenuItem>
-                    <MenuItem onClick={handleLogout}>LogOut</MenuItem>
-                  </Menu>
-                </div>
-              )}
-            </Grid>
-          </Grid>
-        </div>
-
-        <div className={classes.menuWrapper}>
-          <div className="container">
-            <NavLink className={classes.menu} exact to="/">
-              Home
-            </NavLink>
-            <NavLink className={classes.menu} to="/slider">
-              Slider Module
-            </NavLink>
-            <NavLink className={classes.menu} to="/blog-list">
-              Blog Module
-            </NavLink>
-            <NavLink className={classes.menu} to="/contact-us">
-              Contact Module
-            </NavLink>
-            <NavLink className={classes.menu} to="/faq">
-              FAQ Module
-            </NavLink>
-            <NavLink className={classes.menu} to="/subscribe">
-              Susbscribe Module
-            </NavLink>
-            <NavLink className={classes.menu} to="/blog-list">
-              Blogs
-            </NavLink>
+    <header>
+      <div className="container mx-auto">
+        {!token ? (
+          <div>
+            <Button
+              onClick={redirectToRegister}
+              variant="contained"
+              className={classes.button}
+            >
+              Register
+            </Button>
+            <Button
+              onClick={redirectToLogin}
+              variant="contained"
+              className={classes.button}
+            >
+              Login
+            </Button>
           </div>
-        </div>
-      </header>
-    </div>
+        ) : (
+          <div className={classes.topRightWrapper}>
+            <Button className={classes.dropDown} onClick={handleMenu}>
+              <span>{user.name}</span>
+
+              <AccountCircle />
+            </Button>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={open}
+              onClose={handleClose}
+            >
+              {user.isAdmin && (
+                <MenuItem onClick={handleClose}>
+                  <Link
+                    to="/admin/dashboard"
+                    style={{ textDecoration: 'none', color: 'black' }}
+                  >
+                    Dashboard
+                  </Link>
+                </MenuItem>
+              )}
+              <MenuItem onClick={handleClose}>
+                <Link
+                  to="/user/profile"
+                  style={{ textDecoration: 'none', color: 'black' }}
+                >
+                  Profile
+                </Link>
+              </MenuItem>
+              <MenuItem onClick={handleLogout}>LogOut</MenuItem>
+            </Menu>
+          </div>
+        )}
+      </div>
+    </header>
   );
 };
 
