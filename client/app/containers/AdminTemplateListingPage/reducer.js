fbd5d8d8a2ca8c9c098a65b162e4f57610d60bd1
saileshkasaju <leosailesh@gmail.com>
2019-05-05 13:34:30 +0545

    template update done

---
+++ client/app/containers/AdminTemplateListingPage/reducer.js
@@ -0,0 +1,48 @@
+/*
+ *
+ * AdminTemplateListingPage reducer
+ *
+ */
+import produce from 'immer';
+import * as types from './constants';
+
+export const initialState = {
+  all: [],
+  one: {
+    variables: [],
+    template_name: '',
+    template_key: '',
+    information: '',
+    alternate_text: '',
+    form: '',
+    subject: '',
+    body: '',
+  },
+};
+
+/* eslint-disable default-case, no-param-reassign */
+const adminTemplateListingPageReducer = (state = initialState, action) =>
+  produce(state, draft => {
+    switch (action.type) {
+      case types.SET_QUERY_VALUE:
+        draft.query[action.payload.key] = action.payload.value;
+        break;
+      case types.CLEAR_QUERY:
+        draft.query = initialState.query;
+        break;
+      case types.SET_ONE_VALUE:
+        draft.one[action.payload.key] = action.payload.value;
+        break;
+      case types.LOAD_ALL_SUCCESS:
+        draft.all = action.payload.data;
+        break;
+      case types.LOAD_ONE_SUCCESS:
+        draft.one = action.payload.data;
+        break;
+      case types.CLEAR_ONE:
+        draft.one = initialState.one;
+        break;
+    }
+  });
+
+export default adminTemplateListingPageReducer;
---
+++ client/app/containers/AdminTemplateListingPage/tests/selectors.test.js
@@ -0,0 +1,7 @@
+// import { selectAdminTemplateListingPageDomain } from '../selectors';
+
+describe('selectAdminTemplateListingPageDomain', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
