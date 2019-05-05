d96f2300af998abb8d5d1ee3697ddf2e77359f50
saileshkasaju <leosailesh@gmail.com>
2019-05-05 16:24:38 +0545

    fb login implementation test

--- client/app/containers/LoginUserPage/actions.js
+++ client/app/containers/LoginUserPage/actions.js
@@ -16,8 +16,15 @@ export const loginRequest = payload => ({ type: types.LOGIN_REQUEST, payload });
 export const loginSuccess = payload => ({ type: types.LOGIN_SUCCESS, payload });
 export const loginFailure = payload => ({ type: types.LOGIN_FAILURE, payload });
 
-export function defaultAction() {
-  return {
-    type: types.DEFAULT_ACTION,
-  };
-}
+export const loginWithFbRequest = payload => ({
+  type: types.LOGIN_WITH_FB_REQUEST,
+  payload,
+});
+export const loginWithFbSuccess = payload => ({
+  type: types.LOGIN_WITH_FB_SUCCESS,
+  payload,
+});
+export const loginWithFbFailure = payload => ({
+  type: types.LOGIN_WITH_FB_FAILURE,
+  payload,
+});
