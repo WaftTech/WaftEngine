2a32895f2628fa4b3ce64f3f79a3f620baed5275
Nimesh Dahal <famous.me01@gmail.com>
2019-05-08 15:45:42 +0545

    template bug fixed

--- server/modules/template/templateController.js
+++ server/modules/template/templateController.js
@@ -9,15 +9,7 @@ const internal = {};
 templateController.getTemplateName = async (req, res, next) => {
   try {
     const names = await templateSch.find().select('template_name template_key');
-    return otherHelper.sendResponse(
-      res,
-      httpStatus.OK,
-      true,
-      names,
-      null,
-      templateConfig.namesGet,
-      null,
-    );
+    return otherHelper.sendResponse(res, httpStatus.OK, true, names, null, templateConfig.namesGet, null);
   } catch (err) {
     next(err);
   }
@@ -26,31 +18,12 @@ templateController.getTemplateName = async (req, res, next) => {
 templateController.getTemplateDetail = async (req, res, next) => {
   try {
     const template_key = req.params.key;
-    const template = await templateSch.findOne(
-      { template_key, is_deleted: false },
-      'template_name template_key information variables from subject body alternate_text updated_by updated_at',
-    );
+    const template = await templateSch.findOne({ template_key, is_deleted: false }, 'template_name template_key information variables from subject body alternate_text updated_by updated_at');
 
     if (template) {
-      return otherHelper.sendResponse(
-        res,
-        httpStatus.OK,
-        true,
-        template,
-        null,
-        templateConfig.templateGet,
-        null,
-      );
+      return otherHelper.sendResponse(res, httpStatus.OK, true, template, null, templateConfig.templateGet, null);
     } else {
-      return otherHelper.sendResponse(
-        res,
-        httpStatus.NOT_FOUND,
-        false,
-        null,
-        null,
-        templateConfig.templateNotFound,
-        null,
-      );
+      return otherHelper.sendResponse(res, httpStatus.NOT_FOUND, false, null, null, templateConfig.templateNotFound, null);
     }
   } catch (err) {
     next(err);
@@ -59,17 +32,7 @@ templateController.getTemplateDetail = async (req, res, next) => {
 
 templateController.postTemplate = async (req, res, next) => {
   try {
-    const {
-      _id,
-      template_name,
-      template_key,
-      information,
-      variables,
-      from,
-      subject,
-      alternate_text,
-      body,
-    } = req.body;
+    const { _id, template_name, template_key, information, variables, from, subject, alternate_text, body } = req.body;
 
     // if (_id) {
     const update = await templateSch.findOneAndUpdate(
@@ -91,25 +54,9 @@ templateController.postTemplate = async (req, res, next) => {
     );
 
     if (update) {
-      return otherHelper.sendResponse(
-        res,
-        httpStatus.OK,
-        true,
-        req.body,
-        null,
-        templateConfig.templateSave,
-        null,
-      );
+      return otherHelper.sendResponse(res, httpStatus.OK, true, req.body, null, templateConfig.templateSave, null);
     } else {
-      return otherHelper.sendResponse(
-        res,
-        httpStatus.NOT_FOUND,
-        false,
-        null,
-        null,
-        templateConfig.templateNotFound,
-        null,
-      );
+      return otherHelper.sendResponse(res, httpStatus.NOT_FOUND, false, null, null, templateConfig.templateNotFound, null);
     }
     // } else {
     //   const tmeplate = new templateSch({ template_name, template_key, information, variables, from, subject, body });
@@ -136,18 +83,10 @@ internal.renderTemplate = async (template_key, variables_OBJ, toEmail) => {
   let variables_keys = Object.keys(variables_OBJ);
 
   for (let i = 0; i < variables_keys.length; i++) {
-    subject = subject.replace(
-      `%${variables_keys[i]}%`,
-      variables_OBJ[variables_keys[i]],
-    );
-    body = body.replace(
-      `%${variables_keys[i]}%`,
-      variables_OBJ[variables_keys[i]],
-    );
-    alternate_text = alternate_text.replace(
-      `%${variables_keys[i]}%`,
-      variables_OBJ[variables_keys[i]],
-    );
+    let re = new RegExp(`%${variables_keys[i]}%`, 'g');
+    subject = subject.replace(re, variables_OBJ[variables_keys[i]]);
+    body = body.replace(re, variables_OBJ[variables_keys[i]]);
+    alternate_text = alternate_text.replace(re, variables_OBJ[variables_keys[i]]);
   }
   return { from, subject, html: body, text: alternate_text, to: toEmail };
 };
