2e297faa63e8767889aac8dc332bc9f396c74232
jivanshr <jivanshr@gmail.com>
2019-05-12 11:12:02 +0545

    registerpage changes

--- client/app/containers/LoginUserPage/index.js
+++ client/app/containers/LoginUserPage/index.js
@@ -36,65 +36,69 @@ const LoginUserPage = ({
     loginRequest();
   };
   return (
-    <div className="mx-auto max-w-xs pl-2 pr-2 mt-24 mb-24">
-      <h1 className="font-light">LOGIN</h1>
-      <form className="mt-4" onSubmit={handleSubmit}>
-        <UsernameInput />
-        <PasswordInput />
-        <button
-          className="bg-grey-darker hover:bg-grey-dark text-white font-bold py-2 px-4 rounded mt-4 block"
-          type="submit"
+    <div className="container mx-auto pl-2 pr-2 mt-24 mb-24">
+      <div className="max-w-xs">
+        <h1 className="font-light">LOGIN</h1>
+        <form className="mt-4" onSubmit={handleSubmit}>
+          <UsernameInput />
+          <PasswordInput />
+          <button
+            className="bg-grey-darker hover:bg-grey-dark text-white font-bold py-2 px-4 rounded mt-4 block"
+            type="submit"
+          >
+            LOGIN
+          </button>
+        </form>
+
+        <Link
+          className="inline-block align-baseline text-xs text-blue hover:text-blue-darker"
+          to="/signup-user"
         >
-          LOGIN
-        </button>
-      </form>
+          Don't Have Account? Register
+        </Link>
 
-      <Link
-        className="inline-block align-baseline text-xs text-blue hover:text-blue-darker"
-        to="/signup-user"
-      >
-        Don't Have Account? Register
-      </Link>
+        <p className="text-muted text-center mt-10 mb-4 text-xs">
+          OR LOGIN WITH
+        </p>
 
-      <p className="text-muted text-center mt-10 mb-4 text-xs">OR LOGIN WITH</p>
-
-      <div className="mt-5 mb-5 flex space-around">
-        <FacebookLogin
-          appId={FB_APP_ID}
-          textButton="Facebook"
-          autoLoad={false}
-          fields={FB_APP_FIELDS}
-          callback={loginWithFbRequest}
-          containerStyle={{
-            textAlign: 'center',
-            backgroundColor: '#3b5998',
-            borderColor: '#3b5998',
-            flex: 1,
-            color: '#fff',
-            cursor: 'pointer',
-          }}
-          buttonStyle={{
-            flex: 1,
-            textTransform: 'none',
-            padding: '12px',
-            background: 'none',
-            border: 'none',
-            fontSize: '13px',
-          }}
-          icon="fa-facebook"
-        />
-        <GoogleLogin
-          containerStyle={{ boxShadow: 'none', border: '1px solid #ccc' }}
-          buttonStyle={{ boxShadow: 'none', border: '1px solid #ccc' }}
-          className="flex jusitify-center flex-1 shadow-none"
-          clientId={GOOGLE_CLIENT_ID}
-          buttonText="Google"
-          onSuccess={loginWithGoogleRequest}
-          onFailure={err => {
-            console.log('something went wrong!', err);
-          }}
-          cookiePolicy="single_host_origin"
-        />
+        <div className="mt-5 mb-5 flex space-around">
+          <FacebookLogin
+            appId={FB_APP_ID}
+            textButton="Facebook"
+            autoLoad={false}
+            fields={FB_APP_FIELDS}
+            callback={loginWithFbRequest}
+            containerStyle={{
+              textAlign: 'center',
+              backgroundColor: '#3b5998',
+              borderColor: '#3b5998',
+              flex: 1,
+              color: '#fff',
+              cursor: 'pointer',
+            }}
+            buttonStyle={{
+              flex: 1,
+              textTransform: 'none',
+              padding: '12px',
+              background: 'none',
+              border: 'none',
+              fontSize: '13px',
+            }}
+            icon="fa-facebook"
+          />
+          <GoogleLogin
+            className="flex jusitify-center flex-1 shadow-none"
+            clientId={GOOGLE_CLIENT_ID}
+            buttonText="Google"
+            onSuccess={loginWithGoogleRequest}
+            onFailure={err => {
+              console.log('something went wrong!', err);
+            }}
+            cookiePolicy="single_host_origin"
+            containerStyle={{ boxShadow: 'none', border: '1px solid #ccc' }}
+            buttonStyle={{ boxShadow: 'none', border: '1px solid #ccc' }}
+          />
+        </div>
       </div>
     </div>
   );
