14548e49b3114da471c35532ee202df1987e8eb6
Navin Mishra <navinmishra1717@gmail.com>
2019-05-21 16:12:42 +0545

    subscribe and contact added in admin panel

---
+++ client/app/containers/AdminContactListPage/actions.js
@@ -0,0 +1,37 @@
+/*
+ *
+ * AdminContactListPage actions
+ *
+ */
+
+import * as types from './constants';
+
+export const loadAllRequest = payload => ({
+  type: types.LOAD_ALL_REQUEST,
+  payload,
+});
+export const loadAllSuccess = payload => ({
+  type: types.LOAD_ALL_SUCCESS,
+  payload,
+});
+export const loadAllFailure = payload => ({
+  type: types.LOAD_ALL_FAILURE,
+  payload,
+});
+
+export const setQueryValue = payload => ({
+  type: types.SET_QUERY_VALUE,
+  payload,
+});
+export const loadOneRequest = payload => ({
+  type: types.LOAD_ONE_REQUEST,
+  payload,
+});
+export const loadOneSuccess = payload => ({
+  type: types.LOAD_ONE_SUCCESS,
+  payload,
+});
+export const loadOneFailure = payload => ({
+  type: types.LOAD_ONE_FAILURE,
+  payload,
+});
