7955d0b57788c3a5fe89e837e55b4648b299c917
Navin Mishra <navinmishra1717@gmail.com>
2019-05-31 18:52:23 +0545

    profile managed

--- client/app/containers/ProfilePage/constants.js
+++
@@ -1,7 +0,0 @@
-/*
- *
- * ProfilePage constants
- *
- */
-
-export const DEFAULT_ACTION = 'app/ProfilePage/DEFAULT_ACTION';
---
+++ client/app/containers/UserProfilePage/UserPersonalInformation/constants.js
@@ -0,0 +1,11 @@
+export const SET_ONE_VALUE = 'app/UserPersonalInformation/SET_ONE_VALUE';
+
+export const DEFAULT_ACTION = 'app/UserPersonalInformation/DEFAULT_ACTION';
+
+export const LOAD_ONE_REQUEST = 'app/UserPersonalInformation/LOAD_ONE_REQUEST';
+export const LOAD_ONE_SUCCESS = 'app/UserPersonalInformation/LOAD_ONE_SUCCESS';
+export const LOAD_ONE_FAILURE = 'app/UserPersonalInformation/LOAD_ONE_FAILURE';
+
+export const ADD_EDIT_REQUEST = 'app/UserPersonalInformation/ADD_EDIT_REQUEST';
+export const ADD_EDIT_SUCCESS = 'app/UserPersonalInformation/ADD_EDIT_SUCCESS';
+export const ADD_EDIT_FAILURE = 'app/UserPersonalInformation/ADD_EDIT_FAILURE';
