809d0f915d831aabb34a74da0bd01338a4e7fc8f
Navin Mishra <navinmishra1717@gmail.com>
2019-06-12 15:28:22 +0545

    access manage in process

--- server/modules/role/accessShema.js
+++ server/modules/role/accessShema.js
@@ -4,7 +4,7 @@ const schema = mongoose.Schema;
 const accessSchema = new schema({
   module_id: { type: schema.Types.ObjectId, required: true, ref: 'moduleAccess' },
   role_id: { type: schema.Types.ObjectId, required: true, ref: 'roles' },
-  access_type: { type: [schema.Types.ObjectId], required: true },
+  access_type: [{ type: [schema.Types.ObjectId], required: true }],
   is_active: { type: Boolean, required: true, default: true },
   added_at: { type: Date, default: Date.now },
 });
