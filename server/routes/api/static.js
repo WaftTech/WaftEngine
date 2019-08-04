753ba94cd35211b5144b9b0ba7da23eda68f154b
Manoj Mahato <mkmpvtltd@gmail.com>
2019-08-04 16:59:17 +0545

    Remove Unnecessary

--- server/routes/api/static.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const dModule = require('../../modules/staticData/staticController');
-
-router.get('/district/:stateId', dModule.GetDisctrict);
-router.get('/state', dModule.GetStates);
-router.get('/vdc/:districtId', dModule.GetVdcMunicipality);
-
-module.exports = router;
