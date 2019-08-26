72f750067ecabb69914128d541f0e6e74ab869e8
Manoj Mahato <mkmpvtltd@gmail.com>
2019-08-26 13:26:10 +0545

    use Create index false

--- server/app.js
+++ server/app.js
@@ -67,7 +67,7 @@ async function MongoDBConnection(app) {
   await mongoose
     .connect(mongoURI, {
       useNewUrlParser: true,
-      useCreateIndex: true,
+      useCreateIndex: false,
     })
     .then(() => console.log('MongoDB Connected'));
 
