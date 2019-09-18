1843a20ed4c248ab4fc3fb7b789ec0cb8d664da3
Navin Mishra <navinmishra1717@gmail.com>
2019-09-18 12:27:03 +0545

    comment module separartion from blog module

--- server/modules/blog/blogValidation.js
+++ server/modules/blog/blogValidation.js
@@ -22,38 +22,7 @@ validation.sanitize = (req, res, next) => {
   otherHelper.sanitize(req, sanitizeArray);
   next();
 };
-// validation.sanitizeComment = (req, res, next) => {
-//   const sanitizeArray = [
-//     {
-//       field: 'title',
-//       sanitize: {
-//         trim: true,
-//       },
-//     },
-//   ];
-//   otherHelper.sanitize(req, sanitizeArray);
-//   next();
-// };
-// validation.validateComment = (req, res, next) => {
-//   const data = req.body;
-//   const validateArray = [
-//     {
-//       field: 'title',
-//       validate: [
-//         {
-//           condition: 'IsEmpty',
-//           msg: blogConfig.validate.empty,
-//         },
-//       ],
-//     },
-//   ];
-//   const errors = otherHelper.validation(data, validateArray);
-//   if (!isEmpty(errors)) {
-//     return otherHelper.sendResponse(res, httpStatus.BAD_REQUEST, false, null, errors, 'input errors', null);
-//   } else {
-//     next();
-//   }
-// };
+
 validation.validate = (req, res, next) => {
   const data = req.body;
   const validateArray = [
--- server/routes/api/comment.js
+++ server/routes/api/comment.js
@@ -5,10 +5,10 @@ const commentModule = require('../../modules/comment/commentController');
 const { authorization, authentication } = require('../../middleware/authentication.middleware');
 const { sanitizeComment, validateComment } = require('../../modules/comment/commentValidation');
 
-router.get('/comment/:blog', commentModule.GetBlogCommentByBlog);
-router.get('/comment', commentModule.GetBlogComment);
-router.get('/comment/one/:id', commentModule.GetBlogCommentById);
-router.post('/comment', authorization, sanitizeComment, validateComment, commentModule.PostBlogComment);
-router.delete('/comment/:id', authorization, commentModule.DeleteBlogComment);
+router.get('/:blog', commentModule.GetBlogCommentByBlog);
+router.get('/', commentModule.GetBlogComment);
+router.get('/one/:id', commentModule.GetBlogCommentById);
+router.post('/', authorization, sanitizeComment, validateComment, commentModule.PostBlogComment);
+router.delete('/:id', authorization, commentModule.DeleteBlogComment);
 
 module.exports = router;
