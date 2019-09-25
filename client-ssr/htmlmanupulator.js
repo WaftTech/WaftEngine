064d58eb488f0e803b5f8791d234e6801e181f73
Manoj Mahato <mkmpvtltd@gmail.com>
2019-09-25 16:52:04 +0545

    Update changes

--- client-ssr/htmlmanupulator.js
+++ client-ssr/htmlmanupulator.js
@@ -8,13 +8,43 @@ const { head, body } = require('./constant');
 const { requestThirdPartyApi } = require('./helper');
 htmlmanupulator.StaticPages = async (req, res, next) => {
   try {
+    const path = req._parsedUrl.pathname;
+    let description = `mern stack boilerplate open source `;
+    let imagepath = defaultImg;
+    let title = 'Blogs - WaftEngine (MERN Stack Engine)';
+    let url = req._parsedUrl.href;
+    switch (path) {
+      case '/login-user':
+        title = 'Login - WaftEngine (MERN Stack Engine)';
+        break;
+      case '/signup-user':
+        title = 'Sign Up - WaftEngine (MERN Stack Engine)';
+        break;
+      case '/forgot-password-user':
+        title = 'Forgot Password - WaftEngine (MERN Stack Engine)';
+        break;
+      case '/subscribe':
+        title = 'Subscription - WaftEngine (MERN Stack Engine)';
+        break;
+      case '/contact-us':
+        title = 'Contact Us - WaftEngine (MERN Stack Engine)';
+        break;
+      case '/blog':
+        title = 'Blog - WaftEngine (MERN Stack Engine)';
+        break;
+      case '/faq':
+        title = 'Faq - WaftEngine (MERN Stack Engine)';
+        break;
+      default:
+      // code block
+    }
     internal.createHtml(
       res,
       {
-        imagepath: defaultImg,
-        description: `WaftTech Blog, Blog by WaftTech Developer, React, Node, MongoDB, Redux, Development, Tools, WaftEngine, MERN Stack, Eccomerce`,
-        title: 'Blogs - WaftTech (MERN Stack Engine)',
-        url: 'blog-list',
+        imagepath: imagepath,
+        description: description,
+        title: title,
+        url: url,
       },
       next,
     );
@@ -24,29 +54,24 @@ htmlmanupulator.StaticPages = async (req, res, next) => {
 };
 htmlmanupulator.DynamicPages = async (req, res, next) => {
   try {
-    internal.createHtml(
+    const path = req._parsedUrl.pathname;
+    let description = `mern stack boilerplate open source `;
+    let imagepath = defaultImg;
+    let title = 'Blogs - WaftEngine (MERN Stack Engine)';
+    let url = req._parsedUrl.href;
+
+    const slug_url = path;
+    if (slug_url === 'favicon.png') {
+      return res.send(null);
+    }
+    const response = await requestThirdPartyApi(req, `${apiBaseUrl}api/contents/key${slug_url}`, '', next, 'GET');
+    return internal.createHtml(
       res,
       {
-        imagepath: defaultImg,
-        description: `WaftTech Blog, Blog by WaftTech Developer, React, Node, MongoDB, Redux, Development, Tools, WaftEngine, MERN Stack, Eccomerce`,
-        title: 'Blogs - WaftTech (MERN Stack Engine)',
-        url: 'blog-list',
-      },
-      next,
-    );
-  } catch (err) {
-    next(err);
-  }
-};
-htmlmanupulator.FaqPage = async (req, res, next) => {
-  try {
-    internal.createHtml(
-      res,
-      {
-        imagepath: defaultImg,
-        description: `WaftTech Blog, Blog by WaftTech Developer, React, Node, MongoDB, Redux, Development, Tools, WaftEngine, MERN Stack, Eccomerce`,
-        title: 'Blogs - WaftTech (MERN Stack Engine)',
-        url: 'blog-list',
+        imagepath: imagepath,
+        description: response.data && response.data.description ? response.data.description : description,
+        title: response.data && response.data.name ? response.data.name : title,
+        url: url,
       },
       next,
     );
@@ -57,18 +82,22 @@ htmlmanupulator.FaqPage = async (req, res, next) => {
 
 htmlmanupulator.BlogBySlugUrl = async (req, res, next) => {
   try {
-    const id = req.params.slug_url;
-    if (id === 'favicon.png') {
+    const slug_url = req.params.slug_url;
+    let description = `mern stack boilerplate open source `;
+    let imagepath = defaultImg;
+    let title = 'Blogs - WaftEngine (MERN Stack Engine)';
+    let url = req._parsedUrl.href;
+    if (slug_url === 'favicon.png') {
       return res.send(null);
     }
-    const response = await requestThirdPartyApi(req, `${apiBaseUrl}/blog/blog/${id}`, '', next, 'GET');
+    const response = await requestThirdPartyApi(req, `${apiBaseUrl}api/blog/blog/${slug_url}`, '', next, 'GET');
     return internal.createHtml(
       res,
       {
-        imagepath: response.data.image.path,
-        description: response.data.short_description,
-        title: response.data.title,
-        url: `blog/${id}`,
+        imagepath: response.data && response.data.image && response.data.image.path ? '/' + response.data.image.path : imagepath,
+        description: response.data && response.data.short_description ? response.data.short_description : description,
+        title: response.data && response.data.title ? response.data.title : title,
+        url: `/blog/${slug_url}`,
       },
       next,
     );
@@ -78,31 +107,40 @@ htmlmanupulator.BlogBySlugUrl = async (req, res, next) => {
 };
 htmlmanupulator.BlogsByCategoryPages = async (req, res, next) => {
   try {
-    internal.createHtml(
+    const slug_url = req.params.slug_url;
+    let description = `mern stack boilerplate open source `;
+    let imagepath = defaultImg;
+    let title = 'Blogs - WaftEngine (MERN Stack Engine)';
+    let url = req._parsedUrl.href;
+    if (slug_url === 'favicon.png') {
+      return res.send(null);
+    }
+    const response = await requestThirdPartyApi(req, `${apiBaseUrl}api/blog/blogbycat/${slug_url}`, '', next, 'GET');
+    return internal.createHtml(
       res,
       {
-        imagepath: defaultImg,
-        description: `WaftTech, Blog Categories`,
-        title: 'Blog Category - WaftTech (MERN Stack Engine)',
-        url: 'blog-category',
+        imagepath: response.data && response.data[0] && response.data[0].image && response.data[0].image.path ? '/' + response.data[0].image.path : imagepath,
+        description: response.data && response.data[0] && response.data[0].short_description ? response.data[0].short_description : description,
+        title: response.data && response.data[0] && response.data[0].title ? response.data[0].title : title,
+        url: `/blog/${slug_url}`,
       },
       next,
     );
   } catch (err) {
-    next(err);
+    return next(err);
   }
 };
 
 htmlmanupulator.BlogsByTagPages = async (req, res, next) => {
   try {
-    const { id } = req.params;
+    const { tag } = req.params;
     internal.createHtml(
       res,
       {
         imagepath: defaultImg,
-        description: `Blog By Category`,
-        title: 'Blog Category - WaftTech (MERN Stack Engine)',
-        url: `blog-category/${id}`,
+        description: `Blog By ${tag}`,
+        title: `${tag} - WaftEngine (MERN Stack Engine)`,
+        url: `blog/tag/${tag}`,
       },
       next,
     );
@@ -112,14 +150,14 @@ htmlmanupulator.BlogsByTagPages = async (req, res, next) => {
 };
 htmlmanupulator.BlogsByAuthorPages = async (req, res, next) => {
   try {
-    const { id } = req.params;
+    const { author } = req.params;
     internal.createHtml(
       res,
       {
         imagepath: defaultImg,
-        description: `Blog By Category`,
-        title: 'Blog Category - WaftTech (MERN Stack Engine)',
-        url: `blog-category/${id}`,
+        description: `Blog By Author`,
+        title: 'Blog Author - WaftEngine (MERN Stack Engine)',
+        url: `blog/author/${author}`,
       },
       next,
     );
@@ -129,14 +167,18 @@ htmlmanupulator.BlogsByAuthorPages = async (req, res, next) => {
 };
 htmlmanupulator.BlogByDatePages = async (req, res, next) => {
   try {
-    const { id } = req.params;
+    const { date } = req.params;
+    const d = date.split('-');
+    const months = ['', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    console.log(d);
+    const month = months[Number(d[1])];
     internal.createHtml(
       res,
       {
         imagepath: defaultImg,
-        description: `Blog By Category`,
-        title: 'Blog Category - WaftTech (MERN Stack Engine)',
-        url: `blog-category/${id}`,
+        description: `Blogs of ${d[0]} ${month}`,
+        title: `Blog of ${d[0]} ${month} - WaftEngine (MERN Stack Engine)`,
+        url: `blog/date/${date}`,
       },
       next,
     );
@@ -151,7 +193,7 @@ internal.createHtml = (res, data, next) => {
     const text = htmlToText.fromString(description, {
       wordwrap: 130,
     });
-    const html_replacer = `<title>${title}</title><meta property="description" content="${text}" data-react-helmet="true"/><meta name="author" content="Waft Technology Pvt Ltd."/><meta property="image" content="${siteUrl}${imagepath}" data-react-helmet="true"/><meta property="og:image" content="${siteUrl}${imagepath}" data-react-helmet="true"/><meta property="og:title" content="${title}" data-react-helmet="true"/><meta property="og:description" content="${text}" data-react-helmet="true"/><meta property="og:image:secure_url" content="${siteUrl}${imagepath}" data-react-helmet="true"/><meta property="og:url" content="${siteUrl}${url}" data-react-helmet="true"/><link href="https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap" rel="stylesheet"><meta name="robots" content="index, follow"><meta property="og:type" content="website" data-react-helmet="true"/><meta property="fb:app_id" content="261077198152655" data-react-helmet="true"/>`;
+    const html_replacer = `<title>${title}</title><meta property="description" content="${text}" data-react-helmet="true"/><meta name="author" content="Waft Enginenology Pvt Ltd."/><meta property="image" content="${siteUrl}${imagepath}" data-react-helmet="true"/><meta property="og:image" content="${siteUrl}${imagepath}" data-react-helmet="true"/><meta property="og:title" content="${title}" data-react-helmet="true"/><meta property="og:description" content="${text}" data-react-helmet="true"/><meta property="og:image:secure_url" content="${siteUrl}${imagepath}" data-react-helmet="true"/><meta property="og:url" content="${siteUrl}${url}" data-react-helmet="true"/><link href="https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap" rel="stylesheet"><meta name="robots" content="index, follow"><meta property="og:type" content="website" data-react-helmet="true"/><meta property="fb:app_id" content="261077198152655" data-react-helmet="true"/>`;
     const htmlContent = `${head}${html_replacer}${body}`;
     return res.send(htmlContent);
   } catch (err) {
--- client-ssr/route.js
+++ client-ssr/route.js
@@ -2,8 +2,7 @@ const express = require('express');
 const router = express.Router();
 const htmlManupulator = require('./htmlmanupulator');
 
-router.get(['/login-user', '/signup-user', '/forgot-password-user', '/subscribe', '/contact-us', '/blog'], htmlManupulator.StaticPages);
-router.get('/faq', htmlManupulator.FaqPage);
+router.get(['/login-user', '/signup-user', '/forgot-password-user', '/subscribe', '/contact-us', '/blog', '/faq'], htmlManupulator.StaticPages);
 router.get(['/about-us', '/term-and-condition', '/data-policy', '/cookies-policy'], htmlManupulator.DynamicPages);
 router.get('/blog/:slug_url', htmlManupulator.BlogBySlugUrl);
 router.get('/blog/category/:slug_url', htmlManupulator.BlogsByCategoryPages);
