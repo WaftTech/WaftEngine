7f227b1f07d0d0bad8470fd2caefe3cc2ab36c04
Sailesh Kasaju <leosailesh@gmail.com>
2019-09-27 15:32:24 +0545

    add fallback Loading text for static div

--- client/app/components/StaticContentDiv/index.js
+++ client/app/components/StaticContentDiv/index.js
@@ -1,49 +1,13 @@
+/**
+ *
+ * Asynchronously loads the component for Breadcrumb
+ *
+ */
+
 import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import { compose } from 'redux';
-import { makeSelectContent } from '../../containers/App/selectors';
-import { loadContentRequest } from '../../containers/App/actions';
+import loadable from 'utils/loadable';
+import Loading from '../Loading';
 
-/* eslint-disable react/no-danger */
-class StaticContent extends React.PureComponent {
-  static propTypes = {
-    contentKey: PropTypes.string.isRequired,
-    loadContent: PropTypes.func.isRequired,
-    contentObj: PropTypes.object.isRequired,
-  };
-
-  componentDidMount() {
-    if (this.props.contentObj[this.props.contentKey]) {
-      return;
-    }
-    this.props.loadContent(this.props.contentKey);
-  }
-
-  render() {
-    const { contentObj } = this.props;
-
-    if (!contentObj[this.props.contentKey]) return null; // maybe add a loader here
-    return (
-      <div
-        dangerouslySetInnerHTML={{ __html: contentObj[this.props.contentKey] }}
-      />
-    );
-  }
-}
-
-const mapStateToProps = createStructuredSelector({
-  contentObj: makeSelectContent(),
+export default loadable(() => import('./StaticContentDiv'), {
+  fallback: <div>Loading...</div>,
 });
-
-const mapDispatchToProps = dispatch => ({
-  loadContent: payload => dispatch(loadContentRequest(payload)),
-});
-
-const withConnect = connect(
-  mapStateToProps,
-  mapDispatchToProps,
-);
-
-export default compose(withConnect)(StaticContent);
