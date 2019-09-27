5002b1ba82db6126b8b00b053f182b61ff717f97
Sailesh Kasaju <leosailesh@gmail.com>
2019-09-27 15:32:37 +0545

    add fallback Loading text for static div

--- client/app/components/StaticContentDiv/index.js
+++ client/app/components/StaticContentDiv/index.js
@@ -6,7 +6,6 @@
 
 import React from 'react';
 import loadable from 'utils/loadable';
-import Loading from '../Loading';
 
 export default loadable(() => import('./StaticContentDiv'), {
   fallback: <div>Loading...</div>,
