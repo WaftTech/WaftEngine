0519c25a5c46437627bfb57251548831c176d399
Manoj Mahato <mkmpvtltd@gmail.com>
2019-10-25 17:43:24 +0545

    Skeleton Update for Blog category component

--- client/app/components/CategoryElement/skeleton.js
+++ client/app/components/CategoryElement/skeleton.js
@@ -1,29 +1,47 @@
 import React from 'react';
 import Skeleton from 'react-loading-skeleton';
 
-const Loading = () => (
+const Loading = ({ size }) => (
   <>
-    <h2 className="pt-5 pb-5">
+    <h2 className="pt-5 pb-4 text-2xl">
       <Skeleton height={20} width={300} />
     </h2>
-    <div className="recent_blog">
-      {[1, 2, 3].map((each, index) => (
-        <div key={each} className={`mr-4 ${index === 0 ? 'first_item' : ''}`}>
-          <div className="flex max-w-sm mb-5">
-            <Skeleton height={80} width={100} className="imgContainer" />
-
-            <div className="pl-5">
-              <Skeleton height={10} width={100} />
-              <div className="font-bold text-xl block text-black hover:text-waftprimary heading pointer no-underline">
-                <Skeleton count={Math.floor(Math.random() * 2) + 1} width={500} />
+    <div className="-mx-6 article-group">
+      {Array(size - 1)
+        .fill(null)
+        .map((x, i) => i)
+        .map((each, index) => (
+          <div key={each} className={`px-6 mb-8 item-${index + 1}`}>
+            <div className="flex article-container">
+              <div className="article-img-container">
+                <div className="max-w-none object-cover article-img">
+                  <Skeleton
+                    height={300}
+                    className="max-w-none object-cover article-img"
+                  />
+                </div>
               </div>
-              <span className="text-gray-700 text-sm sans-serif">
-                <Skeleton height={10} width={150} />
-              </span>
+              <div className="pl-5 leading-tight article-text">
+                <span className="text-gray-700 text-sm sans-serif article-date">
+                  <Skeleton height={10} width={100} />
+                </span>
+                <div className="font-bold text-xl block text-black hover:text-waftprimary pointer no-underline article-title">
+                  <Skeleton
+                    count={Math.floor(Math.random() * 2) + 1}
+                    width={500}
+                  />
+                </div>
+                <span className="text-gray-700 text-sm sans-serif author-name">
+                  <Skeleton height={10} width={150} />
+                </span>
+
+                <p className="text-gray-600 leading-relaxed short-description">
+                  <Skeleton height={8} count={4} width={150} />
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   </>
 );
