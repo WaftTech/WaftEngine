9723a47d2978f9c7e067fabb97e2331c22338b4e
saileshkandel789 <saileshkandel789@gmail.com>
2019-12-05 15:49:13 +0545

    config updated

--- server/modules/form/formConfig.js
+++ server/modules/form/formConfig.js
@@ -2,7 +2,10 @@ module.exports = {
     validate: {
       empty: 'This field is required',
       nameLength: 'This field should be between 3 to 100',
-      descriptionLength: 'This field should be between 5 to 2000'
+      descriptionLength: 'This field should be between 5 to 2000',
+      email: 'This email field should be valid ',
+      phone: 'This phone field should be valid',
+      boolean : 'This field should be boolean i.e true or false'
     },
     save: 'form saved successfully.',
     get: 'form get successfully.',
