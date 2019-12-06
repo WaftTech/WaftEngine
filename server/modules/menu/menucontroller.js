887a5c10de958f0bf907f8f8aab838e3bfe46a9d
Vikash Sah <mevkashsah@gmail.com>
2019-12-06 12:14:00 +0545

    menu updated

--- server/modules/menu/menucontroller.js
+++ server/modules/menu/menucontroller.js
@@ -1,98 +1,201 @@
 const httpStatus = require('http-status');
 
-const {menusch , menu_item } = require('./menuschema');
+const { menusch, menu_item } = require('./menuschema');
 const otherHelper = require('../../helper/others.helper');
 const menuConfig = require('./menuConfig');
 const menuController = {};
 const menuItemController = {};
 
+menuController.getMenu = async (req, res, next) => {
+  let { page, size, populate, selectq, searchq, sortq } = otherHelper.parseFilters(req, 10, false);
+  searchq = { is_deleted: false };
+  if (req.query.find_title) {
+    searchq = { title: { $regex: req.query.find_title, $options: 'i' }, ...searchq };
+  }
 
+  selectq = 'title key order';
+  let data = await otherHelper.getquerySendResponse(menusch, page, size, sortq, searchq, selectq, next, populate);
+  return otherHelper.paginationSendResponse(res, httpStatus.OK, true, data.data, 'Menu get success!!', page, size, data.totaldata);
+};
 
-menuController.getMenu = async(req, res, next) => {
-    // res.status(200).json({message: 'menu successfully'});
-    try{
-        const menuList = await menusch.find({is_deleted:false});
-        return otherHelper.sendResponse(res, httpStatus.OK, true,menuList ,null,menuConfig.get , null );
-    }catch(err) {
-        next(err);
+menuItemController.getMenuItem = async (req, res, next) => {
+  try {
+    const menuListItem = await menu_item.find();
+    return otherHelper.sendResponse(res, httpStatus.OK, true, menuListItem, null, menuConfig.get, null);
+  } catch (err) {
+    next(err);
+  }
+};
+
+menuItemController.saveMenuItem = async (req, res, next) => {
+  try {
+    let menuitem = req.body;
+    if (menuitem && menuitem._id) {
+      menuitem.updated_at = new Date();
+      menuitem.updated_by = req.user.id;
+      const update = await menu_item.findByIdAndUpdate(
+        menuitem._id,
+        {
+          $set: menuitem,
+        },
+        { new: true },
+      );
+      return otherHelper.sendResponse(res, httpStatus.OK, true, update, null, menuConfig.save, null);
+    } else {
+      menuitem.added_at = new Date();
+      menuitem.added_by = req.user.id;
+      const newMenuItem = new menu_item(menuitem);
+      const menu_item_save = await newMenuItem.save();
+      return otherHelper.sendResponse(res, httpStatus.OK, true, menu_item_save, null, menuConfig.save, null);
     }
-}
+  } catch (err) {
+    next(err);
+  }
+};
 
-menuItemController.getMenuItem = async(req, res, next) => {
-    // res.status(200).json({message: 'menu successfully'});
-    try{
-        const menuListItem = await menu_item.find();
-        return otherHelper.sendResponse(res, httpStatus.OK, true,menuListItem ,null,menuConfig.get , null );
-    }catch(err) {
-        next(err);
+menuController.saveMenu = async (req, res, next) => {
+  try {
+    let menu = req.body;
+    if (menu && menu._id) {
+      menu.updated_by = req.user.id;
+      menu.updated_at = new Date();
+
+      const update = await menusch.findByIdAndUpdate(
+        menu._id,
+        {
+          $set: menu,
+        },
+        { new: true },
+      );
+
+      return otherHelper.sendResponse(res, httpStatus.OK, true, update, null, menuConfig.save, null);
+    } else {
+      menu.added_by = req.user.id;
+      menu.added_at = new Date();
+      const newMenu = new menusch(menu);
+      const MenuSave = await newMenu.save();
+      return otherHelper.sendResponse(res, httpStatus.OK, true, MenuSave, null, menuConfig.save, null);
     }
-}
+  } catch (err) {
+    next(err);
+  }
+};
 
-menuItemController.saveMenuItem = async(req, res, next) => {
-    try{
-        let menuitem = req.body; 
-        if(menuitem && menuitem._id) {
-            menuitem.updated_at = new Date();
-            menuitem.updated_by = req.user.id;
-            const update = await menu_item.findByIdAndUpdate(menuitem._id, {
-                $set:menuitem
-                
-            }, { new:true });
-            return otherHelper.sendResponse(res,httpStatus.OK,true,update, null,menuConfig.save, null);
+// menuController.getEditMenu = async (req, res, next) => {
+//   const menuId = req.params.menuId;
+//   const menu = await menusch.findById(menuId).populate([{ path: 'sub_menu.menu_item', select: 'title' }]);
+//   return otherHelper.sendResponse(res, httpStatus.OK, true, menu, null, menuConfig.get, null);
+// };
 
-        }else{
-            menuitem.added_at = new Date();
-            menuitem.added_by = req.user.id;
-            const newMenuItem = new menu_item(menuitem);
-            const menu_item_save = await newMenuItem.save();
-            return otherHelper.sendResponse(res,httpStatus.OK, true, menu_item_save, null, menuConfig.save, null);
-
-        }
-         
-    }catch(err){
-        next(err)
+menuController.getEditMenu = async (req, res, next) => {
+  const size_default = 10;
+  let page;
+  let size;
+  let searchq = {};
+  if (req.query.page && !isNaN(req.query.page) && req.query.page != 0) {
+    page = Math.abs(req.query.page);
+  } else {
+    page = 1;
+  }
+  if (req.query.size && !isNaN(req.query.size) && req.query.size != 0) {
+    size = Math.abs(req.query.size);
+  } else {
+    size = size_default;
+  }
+  if (req.query.sort) {
+    let sortfield = req.query.sort.slice(1);
+    let sortby = req.query.sort.charAt(0);
+    if (sortby == 1 && !isNaN(sortby) && sortfield) {
+      //one is ascending
+      sortq = sortfield;
+    } else if (sortby == 0 && !isNaN(sortby) && sortfield) {
+      //zero is descending
+      sortq = '-' + sortfield;
+    } else {
+      sortq = '';
     }
-}
+  }
 
-menuController.saveMenu = async(req, res, next) => {
-    try{
-        let menu = req.body;
-        if(menu && menu._id) {
-            menu.updated_by = req.user.id;
-            menu.updated_at = new Date();
-            
-            const update = await menusch.findByIdAndUpdate(menu._id , {
-                $set:menu
-            }, { new:true });
+  const parent = await menusch.findById(req.params.id).select('title key order');
 
-            return otherHelper.sendResponse(res,httpStatus.OK, true, update, null, menuConfig.save, null);
-        }else{
+  let child = await menu_item.aggregate([
+    {
+      $match: {},
+    },
+    {
+      $lookup: {
+        from: 'menu_items',
+        localField: '_id',
+        foreignField: 'parent_menu',
+        as: 'child_menu',
+      },
+    },
+    {
+      $unwind: { path: '$child_menu', preserveNullAndEmptyArrays: true },
+    },
+    {
+      $project: {
+        _id: 1,
+        is_active: 1,
+        order: 1,
+        title: 1,
+        target: 1,
+        url: 1,
+        is_internal: 1,
+        parent_menu: 1,
+        'child_menu._id': { $ifNull: ['$child_menu._id', ''] },
+        'child_menu.is_active': 1,
+        'child_menu.order': 1,
+        'child_menu.title': 1,
+        'child_menu.target': 1,
+        'child_menu.url': 1,
+        'child_menu.is_internal': 1,
+        'child_menu.parent_menu': 1,
+      },
+    },
 
-            // console.log(req.user.id);
-            menu.added_by=req.user.id;
-            menu.added_at = new Date();
-            const newMenu = new menusch(menu);
-            const MenuSave = await newMenu.save();
-            return otherHelper.sendResponse(res, httpStatus.OK, true, MenuSave,null,menuConfig.save, null );
-        }
+    {
+      $lookup: {
+        from: 'menu_items',
+        localField: 'child_menu._id',
+        foreignField: 'parent_menu',
+        as: 'child_menu.child_menu',
+      },
+    },
+    {
+      $group: {
+        _id: '$_id',
+        is_active: { $first: '$is_active' },
+        order: { $first: '$order' },
+        title: { $first: '$title' },
+        url: { $first: '$url' },
+        child_menu: { $push: '$child_menu' },
+        target: { $push: '$target' },
+        is_internal: { $push: 'is_internal' },
+        parent_menu: { $push: 'parent_menu' },
+      },
+    },
 
-    }catch(err){
-        next(err);
-    }
-}
+    {
+      $skip: (page - 1) * size,
+    },
+    {
+      $limit: size,
+    },
+  ]);
+  return otherHelper.sendResponse(res, httpStatus.OK, true, { parent, child }, null, 'Child menu get success!!', null);
+};
 
-menuController.getEditMenu = async(req, res, next) => {
-    const menuId = req.params.menuId;
-    const menu = await menusch.findById(menuId);
-    return otherHelper.sendResponse(res,httpStatus.OK, true,menu, null, menuConfig.get, null);
-}
+menuController.deleteMenu = async (req, res, next) => {
+  const menuId = req.params.id;
+  const menu = await menusch.findByIdAndUpdate(
+    menuId,
+    {
+      $set: { is_deleted: true },
+    },
+    { new: true },
+  );
+  return otherHelper.sendResponse(res, httpStatus.OK, true, menu, null, menuConfig.delete, null);
+};
 
-menuController.deleteMenu = async(req, res, next) => {
-    const menuId = req.body._id;
-    const menu = await menusch.findByIdAndUpdate(menuId , {
-        $set: { is_deleted : true  }
-    } , { new:true});
-    return otherHelper.sendResponse(res,httpStatus.OK, true, menu , null ,menuConfig.delete , null );
-}
-
-module.exports = { menuController,menuItemController  };
+module.exports = { menuController, menuItemController };
--- server/modules/menu/menuschema.js
+++ server/modules/menu/menuschema.js
@@ -3,89 +3,81 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const menuItemSchema = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    is_internal:{
-        type:Boolean,
-        required:true,
-        default: true
-    },
-    is_active: {
-        type:Boolean,
-        default:true,
-        required:true
-    },
-    
-    target:{
-        type:String,
-        required:true,
-        enum:['_blank' , '_self' , '_parent' , '_top']
-    },
-    link:{
-        type:String, 
-        required:true
-    },
-    added_by:{
-        type:Schema.Types.ObjectId,
-        ref: 'users'
-    },
-    updated_at :{
-        type:Date,
-    },
-    updated_by : {
-        type:Schema.Types.ObjectId,
-        ref: 'users'
-    }, 
-    added_at : {
-        type:Date
-    }
-})
+  title: {
+    type: String,
+    required: true,
+  },
+  parent_menu: { type: Schema.Types.ObjectId, required: false, ref: 'menu_item' },
+  is_internal: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+  url: { type: String, required: true },
+  is_active: {
+    type: Boolean,
+    default: true,
+    required: true,
+  },
+  target: {
+    type: String,
+    required: true,
+    enum: ['_blank', '_self', '_parent', '_top'],
+  },
+  added_by: {
+    type: Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  updated_at: {
+    type: Date,
+  },
+  updated_by: {
+    type: Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  added_at: {
+    type: Date,
+  },
+});
 
 const menuSchema = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    is_active : {
-        type:Boolean,
-        required:true,
-        default:true
-    },
-    sub_menu: [ 
-        {
-            menu_item:{
-                type:Schema.Types.ObjectId,
-                ref:  'menu_item'  
-            },
-            is_active:{
-                type:Boolean,
-                default:true
-            }  
-        }]
-    ,
-    is_deleted: {
-        type:Boolean,
-        required:true,
-        default:false
-    },
-    added_by:{
-        type:Schema.Types.ObjectId,
-        ref: 'users'
-    },
-    updated_at :{
-        type:Date,
-    },
-    updated_by : {
-        type:Schema.Types.ObjectId,
-        ref: 'users'
-    },
-    added_at : {
-        type:Date
-    }
-})
-const menusch =  mongoose.model('menusch',menuSchema );
-const menu_item= mongoose.model('menu_item',menuItemSchema );
-module.exports = {menusch , menu_item};
+  title: {
+    type: String,
+    required: true,
+  },
+  key: {
+    type: String,
+    required: true,
+  },
+  order: { type: Number, required: true, default: 1 },
 
+  // parent_menu: { type: Schema.Types.ObjectId, required: false, ref: 'menu_item' },
+  is_active: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+
+  is_deleted: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  added_by: {
+    type: Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  updated_at: {
+    type: Date,
+  },
+  updated_by: {
+    type: Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  added_at: {
+    type: Date,
+  },
+});
+const menusch = mongoose.model('menusch', menuSchema);
+const menu_item = mongoose.model('menu_item', menuItemSchema);
+module.exports = { menusch, menu_item };
