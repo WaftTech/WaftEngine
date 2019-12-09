d08d9fe0eef36779e89348b88a3d37dee8c3e766
hriteek <hriteekbista@gmail.com>
2019-12-09 13:12:22 +0545

    some changes

--- client/app/components/StaticMenu/index.js
+++ client/app/components/StaticMenu/index.js
@@ -13,6 +13,7 @@ import { compose } from 'redux';
 import { makeSelectMenu } from '../../containers/App/selectors';
 import { loadMenuRequest } from '../../containers/App/actions';
 
+import './style.css';
 /* eslint-disable react/prefer-stateless-function */
 class StaticMenu extends React.PureComponent {
   static propTypes = {
@@ -21,11 +22,11 @@ class StaticMenu extends React.PureComponent {
     menuObj: PropTypes.object.isRequired,
   };
 
-  state = { checked: '', anchorEL: null, anchored: null };
+  state = { checked: '' };
 
-  handleToggle = () => {
-    this.state.checked === ''
-      ? this.setState({ checked: 'checked' })
+  handleToggle = value => {
+    this.state.checked !== value
+      ? this.setState({ checked: value })
       : this.setState({ checked: '' });
   };
 
@@ -36,19 +37,21 @@ class StaticMenu extends React.PureComponent {
     this.props.loadMenuRequest(this.props.menuKey);
   }
 
-  getChildElement = parentObj => {
+  getChildElement = (parentObj, depth) => {
     const childContent = [];
     if (parentObj.child_menu.length) {
       parentObj.child_menu.map(childElement => {
         childContent.push(
-          <ul className="relative menu-child text-sm">
+          <ul
+            className={depth == 1 ? 'relative menu-child text-sm' : 'absolute'}
+          >
             <li>
               <Link to={childElement.url} onClick={this.handleToggle}>
                 {childElement.title}
               </Link>
-              {childElement.child_menu &&
-                childElement.child_menu.length &&
-                this.getChildElement(childElement)}
+              {childElement.child_menu && childElement.child_menu.length
+                ? this.getChildElement(childElement)
+                : null}
             </li>
           </ul>,
         );
@@ -57,8 +60,9 @@ class StaticMenu extends React.PureComponent {
     }
   };
 
-  render() {
-    const { checked } = this.state;
+  /*
+          <div className="text-sm nav md:w-full md:text-center lg:w-auto lg:m-auto lg:border-t-0 lg:text-left fadeInDown animated"></div>
+ */ render() {
     const { menuObj } = this.props;
     const data = menuObj[this.props.menuKey];
     if (!data) return null;
@@ -66,40 +70,49 @@ class StaticMenu extends React.PureComponent {
       if (each.is_internal) {
         if (each.child_menu && each.child_menu[0]._id !== '') {
           return (
+            <>
+              <NavLink
+                to="#"
+                className="hidden md:block menu uppercase text-white text-center block no-underline py-2 hover:bg-primary md:text-black md:hover:bg-transparent md:hover:text-primary md:inline-block md:mr-5"
+                onClick={this.handleToggle}
+              >
+                {each.title}
+                {each.child_menu &&
+                  each.child_menu[0]._id !== '' &&
+                  // checked === each.title &&
+                  this.getChildElement(each, 1)}
+              </NavLink>
+              <br />
+            </>
+          );
+        }
+        return (
+          <>
             <NavLink
-              to="#"
+              to={each.url}
               className="hidden md:block menu uppercase text-white text-center block no-underline py-2 hover:bg-primary md:text-black md:hover:bg-transparent md:hover:text-primary md:inline-block md:mr-5"
               onClick={this.handleToggle}
             >
               {each.title}
-              {each.child_menu &&
-                each.child_menu[0]._id !== '' &&
-                checked === 'checked' &&
-                this.getChildElement(each)}
-            </NavLink>
-          );
-        }
-        return (
-          <NavLink
-            to={each.url}
-            className="hidden md:block menu uppercase text-white text-center block no-underline py-2 hover:bg-primary md:text-black md:hover:bg-transparent md:hover:text-primary md:inline-block md:mr-5"
-            onClick={this.handleToggle}
-          >
-            {each.title}
-            {each.child_menu &&
+              {/* {each.child_menu &&
               each.child_menu[0]._id !== '' &&
-              this.getChildElement(each)}
-          </NavLink>
+              this.getChildElement(each)} */}
+            </NavLink>
+            <br />
+          </>
         );
       }
       return (
-        <a
-          className="hidden md:block menu uppercase text-white text-center block no-underline py-2 hover:bg-primary md:text-black md:hover:bg-transparent md:hover:text-primary md:inline-block md:mr-5"
-          href={each.url}
-          target={each.target}
-        >
-          {each.title}
-        </a>
+        <>
+          <a
+            className="hidden md:block menu uppercase text-white text-center block no-underline py-2 hover:bg-primary md:text-black md:hover:bg-transparent md:hover:text-primary md:inline-block md:mr-5"
+            href={each.url}
+            target={each.target}
+          >
+            {each.title}
+          </a>
+          <br />
+        </>
       );
     });
   }
