f36ef536046bb03778263824f25dd6d4c860740e
Vikash Sah <mevkashsah@gmail.com>
2019-12-24 17:07:57 +0545

    error fix

--- client/app/containers/Admin/MenuManage/AddEditPage/index.js
+++ client/app/containers/Admin/MenuManage/AddEditPage/index.js
@@ -209,9 +209,9 @@ const AddEdit = props => {
         </option>
         {category.map(each => (
           <>
-            {/* <option key={each._id} disabled="" value={each._id}>
-              {`${each.title}weeee`}
-            </option> */}
+            <option key={each._id} disabled="" value={each._id}>
+              {`${each.title}`}
+            </option>
             {each.child_menu && each.child_menu.length > 0
               ? //  && each.child_menu[0]._id !== ''
                 (resetChildContent(),
