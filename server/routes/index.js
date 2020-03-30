f9b208b6139476a2ed29523ab901ecde26886ce2
saileshkandel789 <saileshkandel789@gmail.com>
2020-03-30 11:40:20 +0545

    update

--- server/routes/index.js
+++ server/routes/index.js
@@ -52,9 +52,7 @@ router.use('/menu', menuRoutes);
 // All route of Team module
 const teamRoutes = require('./api/team');
 router.use('/team', teamRoutes);
-// All route of MenuItem module
-const menuItemRoutes = require('./api/menuitem');
-router.use('/menuItem' , menuItemRoutes);
+
 // All route of comment module (comments)
 const commentRoutes = require('./api/comment');
 router.use('/comment', commentRoutes);
