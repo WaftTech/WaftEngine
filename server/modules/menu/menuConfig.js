a62c4b4df12879199401466c8ca2828e6f89ae79
saileshkandel789 <saileshkandel789@gmail.com>
2020-03-30 11:00:28 +0545

    conflict

--- server/modules/menu/menuConfig.js
+++ server/modules/menu/menuConfig.js
@@ -1,5 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
   validate: {
     empty: 'This field is required',
     titleLength: 'This field should be between 3 to 100',
@@ -15,22 +14,3 @@ module.exports = {
   cget: 'menu category get successfully.',
   cdelete: 'menu category deleted successfully!!',
 };
-=======
-    validate: {
-      empty: 'This field is required',
-      titleLength: 'This field should be between 3 to 100',
-      descriptionLength: 'This field should be between 5 to 2000',
-      isMongoId: 'Please select an author!!',
-    },
-    save: 'menu saved successfully.',
-    get: 'menus get successfully.',
-    delete: 'menu deleted successfully!!',
-    deleteCat: 'menu Category deleted successfully!!',
-    csave: 'menu category saved successfully.',
-    cget: 'menu category get successfully.',
-    cdelete: 'menu category deleted successfully!!',
-    isave: 'menuItem saved successfully.',
-    iget: 'menuItems get successfully.',
-    idelete: 'menuItem deleted successfully!!',
-  };
->>>>>>> 01e732e032e223958e93321b5837aa002fdf9bec
