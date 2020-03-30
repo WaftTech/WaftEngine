c511d049475ca7b939736acc4205f66b1d598a3b
saileshkandel789 <saileshkandel789@gmail.com>
2020-03-30 11:00:41 +0545

    conflict

--- server/routes/api/team.js
+++ server/routes/api/team.js
@@ -9,17 +9,4 @@ router.post('/', validate, authorization, teamController.saveTeam);
 router.get('/', teamController.getTeam);
 router.delete('/delete_team', authorization, teamController.deleteTeam);
 
-<<<<<<< HEAD
 module.exports = router;
-=======
-
-router.post('/' ,validate, authorization, teamController.saveTeam);
-
-router.get('/' , teamController.getTeam);
-
-router.get('/:id' , teamController.getTeamDetail);
-
-router.delete('/:id' , authorization, teamController.deleteTeam);
-
-module.exports=router;
->>>>>>> 01e732e032e223958e93321b5837aa002fdf9bec
