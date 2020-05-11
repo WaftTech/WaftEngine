1cdb3890c41ab2b2b07b923c1343d1aff2f1cf01
Manoj Kumar Mahato <mkmpvtltd@gmail.com>
2020-05-11 23:39:45 +0545

    Setting added for Latest changes

---
+++ server/config.bak/settings.js
@@ -0,0 +1,8 @@
+module.exports = {
+  public_register_allow: true,
+  public_register_role: ['5bf7ae90736db01f8fa21a24'],
+  public_register_email_template: 'user_registration',
+  public_register_oauth_template: 'third_party_signup',
+  verify_mail_template: 'verify_mail',
+  force_allow_email_verify: true,
+};
