8cdcab6311f1e4ffe68c3867ce209d4eda6e1a3a
hriteek <hriteekbista@gmail.com>
2020-09-02 15:58:15 +0545

    ✍ UPDATE : Enable both email and google authentication while logging in

--- client/app/containers/LoginUserPage/selectors.js
+++ client/app/containers/LoginUserPage/selectors.js
@@ -42,6 +42,17 @@ export const makeSelectPasswordError = () =>
     state => state.password,
   );
 
+export const makeSelectTwoFactor = () =>
+  createSelector(
+    selectLoginUserPageDomain,
+    state => state.twoFactor,
+  );
+export const makeSelectHelperObj = () =>
+  createSelector(
+    selectLoginUserPageDomain,
+    state => state.helperObj,
+  );
+
 /**
  * Default selector used by LoginUserPage
  */
