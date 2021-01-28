f645d1180dae3ba2b499450e71d73c6c45292962
Ishwor Dangol <prataya2@gmail.com>
2021-01-28 10:38:08 +0545

    module access design update

--- client/app/containers/Admin/Module/AddEditPage/components/Path.js
+++ client/app/containers/Admin/Module/AddEditPage/components/Path.js
@@ -5,6 +5,7 @@ import { withRouter } from 'react-router-dom';
 import { compose } from 'redux';
 
 import { FaRegTrashAlt } from 'react-icons/fa';
+import lid from '../../../../../assets/img/lid.svg';
 
 const methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'];
 
@@ -39,16 +40,20 @@ const Path = props => {
           />
         </div>
         <div className="flex justify-center w-8 text-center h-8 mt-6">
-          <button onClick={handleRemovePath(pathIndex)}>
-            <FaRegTrashAlt className="text-red-500" />
-          </button>
+          <span
+            className="w-8 h-8 inline-flex justify-center items-center leading-none cursor-pointer hover:bg-red-100 rounded-full relative trash-icon"
+            onClick={handleRemovePath(pathIndex)}
+          >
+            <img className="trash-lid" src={lid} alt="trash-id" />
+            <span className="w-3 h-3 rounded-b-sm bg-red-500 mt-1" />
+          </span>
         </div>
       </div>
 
       <div className="flex justify-between">
         <div className="clientRoute w-1/2 border-r">
           <div className="flex mb-2">
-          <label className="label uppercase tracking-wide text-gray-800 text-xs pl-4">Client Route</label>
+            <label className="label uppercase tracking-wide text-gray-800 text-xs pl-4">Client Route</label>
           </div>
           {(each.admin_routes || []).map((eachAdminRoute, index) => (
             <div
@@ -57,7 +62,7 @@ const Path = props => {
             >
               <div className="flex items-center">
                 <input
-                  className="inputbox mr-2"
+                  className="flex-1 inputbox mr-2"
                   id={`${each._id}-each-admin-route-access-type-${index}`}
                   type="text"
                   value={eachAdminRoute}
@@ -66,11 +71,13 @@ const Path = props => {
                 />
 
                 <span
-                  aria-label="Delete client route"
+                  className="w-8 h-8 inline-flex justify-center items-center leading-none cursor-pointer hover:bg-red-100 rounded-full relative trash-icon"
                   onClick={handleRemoveAdminRoute(pathIndex, index)}
                 >
-                  <FaRegTrashAlt className="text-red-500 cursor-pointer mr-2 mt-1" />
+                  <img className="trash-lid" src={lid} alt="trash-id" />
+                  <span className="w-3 h-3 rounded-b-sm bg-red-500 mt-1" />
                 </span>
+
               </div>
             </div>
           ))}
@@ -95,9 +102,8 @@ const Path = props => {
           {(each.server_routes || []).map((eachServerRoute, index) => (
             <div
               className="w-full pb-1 pl-4"
-              key={`${each._id}-${pathIndex}-${
-                eachServerRoute._id
-              }-each-server-route-${index}`}
+              key={`${each._id}-${pathIndex}-${eachServerRoute._id
+                }-each-server-route-${index}`}
             >
               <div className="flex">
                 <div className="mr-2">
@@ -108,17 +114,15 @@ const Path = props => {
                     onChange={handleServerRoutesMethodChange(pathIndex, index)}
                     inputprops={{
                       name: 'Method',
-                      id: `${each._id}-${
-                        eachServerRoute._id
-                      }-each-server-route-${index}-method`,
+                      id: `${each._id}-${eachServerRoute._id
+                        }-each-server-route-${index}-method`,
                     }}
                     style={{ minWidth: '80px', background: '#FFFFFF' }}
                   >
                     {methods.map(eachMethod => (
                       <option
-                        key={`${eachMethod._id}-${pathIndex}-${
-                          eachServerRoute._id
-                        }-each-server-route-method-${eachMethod}`}
+                        key={`${eachMethod._id}-${pathIndex}-${eachServerRoute._id
+                          }-each-server-route-method-${eachMethod}`}
                         value={eachMethod}
                       >
                         {eachMethod}
@@ -131,15 +135,18 @@ const Path = props => {
                     type="text"
                     className="inputbox mr-2 flex-1"
                     style={{ background: '#FFFFFF' }}
-                    id={`${each._id}-${
-                      eachServerRoute._id
-                    }-each-admin-server-route-route-access-type-${index}`}
+                    id={`${each._id}-${eachServerRoute._id
+                      }-each-admin-server-route-route-access-type-${index}`}
                     value={eachServerRoute.route}
                     onChange={handleServerRoutesRouteChange(pathIndex, index)}
                   />
-                  <span onClick={handleRemoveServerRoute(pathIndex, index)}>
-                    <FaRegTrashAlt className="text-red-500 cursor-pointer mr-2 mt-1" />
-                  </span>
+                   <span
+                  className="w-8 h-8 inline-flex justify-center items-center leading-none cursor-pointer hover:bg-red-100 rounded-full relative trash-icon"
+                  onClick={handleRemoveServerRoute(pathIndex, index)}
+                >
+                  <img className="trash-lid" src={lid} alt="trash-id" />
+                  <span className="w-3 h-3 rounded-b-sm bg-red-500 mt-1" />
+                </span>
                 </div>
               </div>
             </div>
