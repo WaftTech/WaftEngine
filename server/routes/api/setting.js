45f4f65fc19d21b678be497c1b05b2a2e56c1c94
Buddhi Raj Paudel <brajpaudel07@gmail.com>
2021-02-08 15:17:10 +0545

    global settings edited

--- server/routes/api/setting.js
+++ server/routes/api/setting.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const settingModule = require('../../modules/setting/settingController');
 const { authentication, authorization } = require('../../middleware/auth.middleware');
+const validation = require('./../../modules/setting/settingValidation')
 
 router.get('/all', authentication, authorization, settingModule.GetSettingAll);
 router.get('/type/:type', authentication, authorization, settingModule.GetSettingType);
-router.get('/single/:setting_id', authentication, settingModule.GetSettingSingle);
-router.post('/edit/all', authentication, authorization, settingModule.EditSetting);
-router.post('/:type', authentication, authorization, settingModule.SaveSetting);
+router.get('/single/:setting_id', authentication, authorization, settingModule.GetSettingSingle);
+// router.post('/edit/all', authentication, authorization, settingModule.EditSetting);
+router.post('/:type', authentication, authorization, validation.validate, settingModule.SaveSetting);
 
 module.exports = router;
