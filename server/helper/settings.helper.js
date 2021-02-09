856d6329b9fe1ad46166f3a50a8a63222703ee1f
Buddhi Raj Paudel <brajpaudel07@gmail.com>
2021-02-09 16:49:13 +0545

    cache memory done

--- server/helper/settings.helper.js
+++ server/helper/settings.helper.js
@@ -1,6 +1,23 @@
 
 const settingSch = require('../modules/setting/settingSchema')
+const NodeCache = require("node-cache");
+const myCache = new NodeCache();
+
 module.exports = async (type, key) => {
-  const setting = await settingSch.findOne({ key: key, type: type }, { value: 1, _id: 0 });
-  return setting.value;
+
+  const temp = type + '_' + key
+  const value = myCache.get(temp);
+  if (value == undefined) {
+    const setting = await settingSch.findOne({ key: key, type: type }, { value: 1, key: 1, _id: 0 });
+    if (setting.value) {
+      myCache.set(temp, setting.value)
+      return setting.value;
+    } else {
+      return null
+    }
+  } else {
+    return value
+  }
 };
+
+
