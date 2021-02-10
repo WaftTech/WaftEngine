42c13574b53a8c06c6f3ea4998eebd30e9a338a5
jivanshr <jivanshr@gmail.com>
2021-02-10 15:33:40 +0545

    Merge branch 'brp' of gitlab.com:wafttechnology/inhouse/waft-engine into jivan

--- server/helper/settings.helper.js
+++ server/helper/settings.helper.js
@@ -5,7 +5,7 @@ const myCache = new NodeCache();
 
 module.exports = async (type, sub_type, key) => {
 
-  const temp = type + '_' + key
+  const temp = type + '_' + sub_type + '_' + key
   const value = myCache.get(temp);
   if (value == undefined) {
     const setting = await settingSch.findOne({ key: key, type: type, sub_type: sub_type }, { value: 1, key: 1, _id: 0 });
