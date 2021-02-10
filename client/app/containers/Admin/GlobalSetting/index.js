5bcf0f9f801123615db1ee536d6db25bd12fe011
jivanshr <jivanshr@gmail.com>
2021-02-10 16:45:53 +0545

    quick fix

--- client/app/containers/Admin/GlobalSetting/index.js
+++ client/app/containers/Admin/GlobalSetting/index.js
@@ -165,12 +165,6 @@ export const GlobalSetting = props => {
                   </option>
                 ))}
             </select>
-            <span
-              className=" inline-flex border-l absolute right-0 top-0 h-8 px-2 mt-1 items-center cursor-pointer text-blue-500"
-              onClick={handleSearch}
-            >
-              <FaSearch />
-            </span>
           </div>
           <div className="flex relative mr-2">
             <select
@@ -194,15 +188,9 @@ export const GlobalSetting = props => {
                     ))}
                 </>
               ) : (
-                <option value="">Choose type first</option>
-              )}
+                  <option value="">Choose type first</option>
+                )}
             </select>
-            <span
-              className=" inline-flex border-l absolute right-0 top-0 h-8 px-2 mt-1 items-center cursor-pointer text-blue-500"
-              onClick={handleSearch}
-            >
-              <FaSearch />
-            </span>
           </div>
 
           <div className="flex relative">
