8f5335213c56d330cdcfe4a63fd5eb9b13eb20b7
jivanshr <jivanshr@gmail.com>
2021-02-14 18:01:19 +0545

    mix changes

--- client/app/containers/HomePage/index.js
+++ client/app/containers/HomePage/index.js
@@ -15,15 +15,6 @@ import StaticContentDiv from '../../components/StaticContentDiv';
 
 /* eslint-disable react/prefer-stateless-function */
 export default class HomePage extends React.PureComponent {
-  state = { open: false };
-
-  handleOpen = () => {
-    this.setState({ open: true });
-  };
-
-  handleClose = () => {
-    this.setState({ open: false });
-  };
 
   render() {
     const { classes, category } = this.props;
@@ -31,9 +22,8 @@ export default class HomePage extends React.PureComponent {
     return (
       <>
         <div className="container mx-auto">
-          <SlickSlider slideKey="homeslider" />
-          <StaticContentDiv contentKey="test" />
-        </div>
+          <SlickSlider slideKey="homeSlider" />
+          <StaticContentDiv contentKey="sdafsdf" /></div>
       </>
     );
   }
