cb5df636bd8ecd970381d77da783cf40f3fad28e
Nilej <nilez.sth@gmail.com>
2021-02-14 16:39:21 +0545

    error boundary changes

--- client/app/components/ErrorBoundary/index.js
+++ client/app/components/ErrorBoundary/index.js
@@ -6,6 +6,7 @@
 
 import PropTypes from 'prop-types';
 import React from 'react';
+import Logo from '../../assets/img/logo-icon.svg';
 
 /* eslint-disable react/prefer-stateless-function */
 class ErrorBoundary extends React.Component {
@@ -21,17 +22,43 @@ class ErrorBoundary extends React.Component {
     // logErrorToMyService(error, info);
   }
 
+  handleGoBack = () => {
+    window.history.back();
+    setInterval(this.handleReload, 200);
+  };
+
+  handleReload = () => {
+    window.location.reload();
+  };
+
   render() {
     const { hasError, error, info } = this.state;
     const { customErrorView } = this.props;
     if (hasError) {
-      if (customErrorView) return customErrorView(error, info);
+      // if (customErrorView) return customErrorView(error, info);
       return (
-        <div>
-          <h1>Something went wrong.</h1>
-          <p>{this.state.info}</p>
-          <pre>{JSON.stringify(this.state.error)}</pre>
-        </div>
+        <>
+          <div className="flex justify-center mt-24">
+            <img src={Logo} className="w-48" />
+          </div>
+          <div className="flex justify-center">
+            <h1>Something went wrong.</h1>
+          </div>
+          <div className="flex justify-center">
+            <button
+              className="underline text-secondary hover:cursor p-2 m-2"
+              onClick={this.handleGoBack}
+            >
+              Go Back.
+            </button>
+            <button
+              className="underline text-secondary hover:cursor p-2 m-2"
+              onClick={this.handleReload}
+            >
+              Relaod.
+            </button>
+          </div>
+        </>
       );
     }
     return this.props.children;
