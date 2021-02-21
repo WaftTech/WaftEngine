48ef68a5c793df6d7c7a2b4eb0d83262a95a6821
Nilej <nilez.sth@gmail.com>
2021-02-21 12:47:43 +0545

    changes

--- client/app/containers/Admin/Dashboard/Charts/BarChart.js
+++ client/app/containers/Admin/Dashboard/Charts/BarChart.js
@@ -26,10 +26,10 @@ const BarChartComponent = ({
       <YAxis />
       <Tooltip />
       <Legend />
-      {key1 && <Bar dataKey={key1} fill="#8884d8" />}
-      {key2 && <Bar dataKey={key2} fill="#82ca9d" />}
-      {key3 && <Bar dataKey={key3} fill="#e97171" />}
-      {key4 && <Bar dataKey={key4} fill="#9d65c9" />}
+      {key1 && <Bar dataKey={key1} barSize={20} fill="#8884d8" />}
+      {key2 && <Bar dataKey={key2} barSize={20} fill="#82ca9d" />}
+      {key3 && <Bar dataKey={key3} barSize={20} fill="#e97171" />}
+      {key4 && <Bar dataKey={key4} barSize={20} fill="#9d65c9" />}
     </BarChart>
   );
 };
