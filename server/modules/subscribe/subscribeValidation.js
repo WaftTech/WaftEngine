1bf80fdc072b4a58c8e88f072c60f120d6a7f8c1
Buddhi Raj Paudel <brajpaudel07@gmail.com>
2021-02-25 11:16:32 +0545

    subscribe fixed

--- server/modules/subscribe/subscribeValidation.js
+++ server/modules/subscribe/subscribeValidation.js
@@ -44,15 +44,12 @@ validations.validate = async (req, res, next) => {
   ]
 
   let errors = validateHelper.validation(data, validateArray);
-  console.log('aaaaa', data)
-  let subscribe_filter = { is_deleted: false, email: data.email }
-  if (data._id) {
-    subscribe_filter = { ...subscribe_filter, _id: { $ne: data._id } }
-  }
+  let subscribe_filter = { is_deleted: true, email: data.email }
+
   const already_subscribe = await subscribeSch.findOne(subscribe_filter);
 
   if (already_subscribe && already_subscribe._id) {
-    errors = { ...errors, subscribe: 'This email has already been subscribed! Thank You!!' }
+    errors = { ...errors, subscribe: 'You cannot subscribe anymore' }
   }
   if (!isEmpty(errors)) {
     return otherHelper.sendResponse(res, httpStatus.BAD_REQUEST, false, null, errors, 'invalid input', null);
