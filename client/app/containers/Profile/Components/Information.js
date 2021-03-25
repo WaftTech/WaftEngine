6dcf99a8135beafc0ee00bd8b657b86eb7424d7e
Nilej <nilez.sth@gmail.com>
2021-03-25 10:27:21 +0545

    profile picture in image

--- client/app/containers/Profile/Components/Information.js
+++ client/app/containers/Profile/Components/Information.js
@@ -2,7 +2,7 @@
 import { push } from 'connected-react-router';
 import moment from 'moment';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import DatePicker from 'react-datepicker';
 import Dropzone from 'react-dropzone';
 
@@ -12,7 +12,7 @@ import { compose } from 'redux';
 import { createStructuredSelector } from 'reselect';
 import injectReducer from 'utils/injectReducer';
 import injectSaga from 'utils/injectSaga';
-import { DATE_FORMAT } from '../../App/constants';
+import { DATE_FORMAT, IMAGE_BASE } from '../../App/constants';
 import * as mapDispatchToProps from '../actions';
 // core components
 import reducer from '../reducer';
@@ -24,45 +24,48 @@ import {
 } from '../selectors';
 import DateInput from '../../../components/DateInput';
 
-class UserPersonalInformationPage extends React.Component {
-  static propTypes = {
-    loadOneRequest: PropTypes.func.isRequired,
-    addEditRequest: PropTypes.func.isRequired,
-    setOneValue: PropTypes.func.isRequired,
-    match: PropTypes.shape({
-      params: PropTypes.object,
-    }),
-    one: PropTypes.object.isRequired,
-    errors: PropTypes.object,
-  };
+const UserPersonalInformationPage = props => {
+  const {
+    classes,
+    one,
+    errors,
+    loading,
+    clearError,
+    loadOneRequest,
+    setOneValue,
+  } = props;
 
-  componentDidMount() {
-    this.props.clearError();
-    this.props.loadOneRequest();
-  }
+  useEffect(() => {
+    clearError();
+    loadOneRequest();
+  }, []);
 
-  state = {
-    image: '',
-  };
+  useEffect(() => {
+    if (one.image && one.image.path) {
+      setImage(`${IMAGE_BASE}${one.image.path}`);
+    }
+  }, [one]);
 
-  handleChange = name => event => {
+  const [image, setImage] = useState('');
+
+  const handleChange = name => event => {
     event.persist();
-    this.props.setOneValue({ key: name, value: event.target.value });
+    setOneValue({ key: name, value: event.target.value });
   };
 
-  handleDateChange = name => date => {
-    this.props.setOneValue({
+  const handleDateChange = name => date => {
+    setOneValue({
       key: name,
       value: moment(date).format('YYYY-MM-DD'),
     });
   };
 
-  handleSave = () => {
-    this.props.addEditRequest();
+  const handleSave = () => {
+    addEditRequest();
   };
 
-  onHandleUpload = files => {
-    this.props.setOneValue({
+  const onHandleUpload = files => {
+    setOneValue({
       key: 'image',
       value: files[0],
     });
@@ -70,119 +73,123 @@ class UserPersonalInformationPage extends React.Component {
     reader.addEventListener(
       'load',
       () => {
-        this.setState({ image: reader.result });
+        setImage(reader.result);
       },
       false,
     );
     reader.readAsDataURL(files[0]);
   };
 
-  render() {
-    const { classes, one, errors, loading } = this.props;
-    return loading ? (
-      <div className="circular_loader waftloader"></div>
-    ) : (
-      <React.Fragment>
-        <div className="flex flex-wrap">
-          <div className="w-full lg:flex-1">
-            <div className="w-full md:w-1/2 pb-4">
-              <label>Name</label>
-              <input
-                className="inputbox"
-                id="name"
-                type="text"
-                name="Name"
-                value={one.name || ''}
-                onChange={this.handleChange('name')}
-              />
-              <div className="error">{errors.name}</div>
-            </div>
+  return loading ? (
+    <div className="circular_loader waftloader"></div>
+  ) : (
+    <React.Fragment>
+      <div className="flex flex-wrap">
+        <div className="w-full lg:flex-1">
+          <div className="w-full md:w-1/2 pb-4">
+            <label>Name</label>
+            <input
+              className="inputbox"
+              id="name"
+              type="text"
+              name="Name"
+              value={one.name || ''}
+              onChange={handleChange('name')}
+            />
+            <div className="error">{errors.name}</div>
+          </div>
 
-            <div className="w-full md:w-1/2 pb-4">
-              <label>Email</label>
-              <input
-                className="inputbox"
-                id="email"
-                type="text"
-                name="Email"
-                value={one.email || ''}
-                onChange={this.handleChange('name')}
-              />
-              <div className="error">{errors.email}</div>
-            </div>
+          <div className="w-full md:w-1/2 pb-4">
+            <label>Email</label>
+            <input
+              className="inputbox"
+              id="email"
+              type="text"
+              name="Email"
+              value={one.email || ''}
+              onChange={handleChange('name')}
+            />
+            <div className="error">{errors.email}</div>
+          </div>
 
-            <div className="md:w-1/2 pb-4">
-              <label className="text-sm">Date Of Birth</label>
-              <DateInput
-                onDateChange={date => {
-                  this.props.setOneValue({
-                    key: 'date_of_birth',
-                    value: moment(date).format('YYYY-MM-DD'),
-                  });
-                }}
-                birth_date={moment(one.date_of_birth).format('YYYY-MM-D')}
-              />
-            </div>
+          <div className="md:w-1/2 pb-4">
+            <label className="text-sm">Date Of Birth</label>
+            <DateInput
+              onDateChange={date => {
+                props.setOneValue({
+                  key: 'date_of_birth',
+                  value: moment(date).format('YYYY-MM-DD'),
+                });
+              }}
+              birth_date={moment(one.date_of_birth).format('YYYY-MM-D')}
+            />
+          </div>
 
-            <div className="w-full pb-4">
-              <div>
-                <label className="text-sm">Role :</label>{' '}
-                {one.roles.map(each => (
-                  <span
-                    key={each._id}
-                    className="rounded-full px-2 py-1 mr-2 text-xs border"
-                  >
-                    {each.role_title}{' '}
-                  </span>
-                ))}
-              </div>
+          <div className="w-full pb-4">
+            <div>
+              <label className="text-sm">Role :</label>{' '}
+              {one.roles.map(each => (
+                <span
+                  key={each._id}
+                  className="rounded-full px-2 py-1 mr-2 text-xs border"
+                >
+                  {each.role_title}{' '}
+                </span>
+              ))}
             </div>
+          </div>
 
-            {/* <div className="w-full  pb-4">
+          {/* <div className="w-full  pb-4">
           Your account created at {moment(one.added_at).format(DATE_FORMAT)}
         </div> */}
-          </div>
-          <div
-            style={{
-              display: 'flex',
-            }}
-          >
-            <Dropzone onDrop={this.onHandleUpload}>
-              {({ getRootProps, getInputProps }) => (
-                <section
-                  style={{ width: '100%' }}
-                  className="text-black  hover:text-primary text-center self-start  border border-gray-500 rounded-lg border-solid cursor-pointer"
-                >
-                  <div {...getRootProps()}>
-                    <input {...getInputProps()} />
-                    {this.state.image ? (
-                      <img
-                        className=" w-full "
-                        src={this.state.image}
-                        alt="profile"
-                      />
-                    ) : (
-                      <div className="p-6">
-                        <p>Choose Profile picture</p>
-                      </div>
-                    )}
-                  </div>
-                </section>
-              )}
-            </Dropzone>
-          </div>
         </div>
-
-        <button
-          className="block btn text-white bg-blue-500 border border-blue-600 hover:bg-blue-600"
-          onClick={this.handleSave}
+        <div
+          style={{
+            display: 'flex',
+          }}
         >
-          Save Changes
-        </button>
-      </React.Fragment>
-    );
-  }
-}
+          <Dropzone onDrop={onHandleUpload}>
+            {({ getRootProps, getInputProps }) => (
+              <section
+                style={{ width: '100%' }}
+                className="text-black  hover:text-primary text-center self-start  border border-gray-500 rounded-lg border-solid cursor-pointer"
+              >
+                <div {...getRootProps()}>
+                  <input {...getInputProps()} />
+                  {image ? (
+                    <img className=" w-full " src={image} alt="profile" />
+                  ) : (
+                    <div className="p-6">
+                      <p>Choose Profile picture</p>
+                    </div>
+                  )}
+                </div>
+              </section>
+            )}
+          </Dropzone>
+        </div>
+      </div>
+
+      <button
+        className="block btn text-white bg-blue-500 border border-blue-600 hover:bg-blue-600"
+        onClick={handleSave}
+      >
+        Save Changes
+      </button>
+    </React.Fragment>
+  );
+};
+
+UserPersonalInformationPage.propTypes = {
+  loadOneRequest: PropTypes.func.isRequired,
+  addEditRequest: PropTypes.func.isRequired,
+  setOneValue: PropTypes.func.isRequired,
+  match: PropTypes.shape({
+    params: PropTypes.object,
+  }),
+  one: PropTypes.object.isRequired,
+  errors: PropTypes.object,
+};
 
 const mapStateToProps = createStructuredSelector({
   one: makeSelectOne(),
