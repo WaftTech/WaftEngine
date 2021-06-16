ad7ca784c58020f440d7acab5e3d18b0805967af
Nilej <nilez.sth@gmail.com>
2021-06-16 11:03:16 +0545

    Dialog component update (render-upper div of app)

---
+++ client/app/components/Dialog/Modal.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Modal = React.memo(({ children, closeModal }) => {
+  const domEl = document.getElementById('modal-root');
+
+  if (!domEl) return null;
+
+  // This is where the magic happens -> our modal div will be rendered into our 'modal-root' div, no matter where we
+  // use this component inside our React tree
+  return ReactDOM.createPortal(<div>{children}</div>, domEl);
+});
+
+export default Modal;
